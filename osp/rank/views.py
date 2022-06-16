a16bd91c28fdafb9c99a657d2a68b9f33e1acf75
tsfo1489 <tsfo1489@gmail.com>
2022-06-16 13:51:50 +0900

    Feat: Add Student Contributor panel

--- osp/rank/views.py
+++ osp/rank/views.py
@@ -1,6 +1,7 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-from user.models import ScoreTable
-from repository.models import GithubRepoStats
+from user.models import ScoreTable, StudentTab
+from repository.models import GithubRepoStats, GithubRepoContributor, GithubRepoCommits, GithubIssues, GithubPulls
 # Create your views here.
 def user_rank(request):
     # Split by Year
@@ -23,5 +24,47 @@ def user_rank(request):
     return render(request, 'rank/user_rank.html', {'data': student_list})
 
 def repo_rank(request):
-    repo_list = list(GithubRepoStats.objects.all())
-    return render(request, 'rank/repo_rank.html', {'data': repo_list})   
+    repo_list = []
+    student_contributor = {}
+    for x in GithubRepoContributor.objects.all():
+        if x.owner_id + '/' + x.repo_name not in student_contributor:
+            student_contributor[x.owner_id + '/' + x.repo_name] = []
+        student_contributor[x.owner_id + '/' + x.repo_name].append(x)
+    for repo in GithubRepoStats.objects.all():
+        repo_link = repo.github_id + '/' + repo.repo_name
+        if repo_link in student_contributor:
+            repo.student_contributor = student_contributor[repo_link]
+            repo_list.append(repo)
+    return render(request, 'rank/repo_rank.html', {'data': repo_list})
+
+def repo_api(request):
+    owner_id = request.GET['github_id']
+    repo_name = request.GET['repo_name']
+    result = []
+    for student in GithubRepoContributor.objects.filter(owner_id=owner_id).filter(repo_name=repo_name):
+        student_profile = list(StudentTab.objects.filter(github_id=student.github_id))
+        if len(student_profile) != 1:
+            continue
+        commmit_cnt = len(GithubRepoCommits.objects.filter(
+            github_id=owner_id,
+            repo_name=repo_name,
+            committer_github=student.github_id
+        ))
+        issue_cnt = len(GithubIssues.objects.filter(
+            owner_id=owner_id,
+            repo_name=repo_name,
+            github_id=student.github_id
+        ))
+        pull_cnt = len(GithubPulls.objects.filter(
+            owner_id=owner_id,
+            repo_name=repo_name,
+            github_id=student.github_id
+        ))
+        result.append({
+            'name': student_profile[0].name,
+            'github_id': student.github_id,
+            'commit_cnt': commmit_cnt,
+            'issue_cnt': issue_cnt,
+            'pull_cnt': pull_cnt
+        })
+    return JsonResponse(result, safe=False)
