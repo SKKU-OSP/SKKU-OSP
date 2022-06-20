1662cd1ed2a3fbb3ffef24be1944f6817f6663c3
tsfo1489 <tsfo1489@gmail.com>
2022-06-20 20:53:04 +0900

    Fix: Fix login error message
     * 로그인 실패시 오류 메시지 수정
     * 로그인시 field 알람 설정

--- osp/osp/urls.py
+++ osp/osp/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -22,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('rank/', include('rank.urls')),
     path('accounts/', include('common.urls')),
-    path('user/', include('user.urls'))
+    path('user/', include('user.urls')),
+    path('', lambda req: redirect('/accounts/login'))
 ]
