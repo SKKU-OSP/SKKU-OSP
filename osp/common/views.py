3526089764f90f9f503f1d571bc1d55bdf679cd2
tsfo1489 <tsfo1489@gmail.com>
2022-06-21 14:53:28 +0900

    Fix: Fix register error
     * User 패스워드 암호화 해결

--- osp/common/views.py
+++ osp/common/views.py
@@ -31,7 +31,8 @@ def register_page(request):
         
         if len(fail_reason) > 0:
             return JsonResponse({'status': 'fail', 'message': fail_reason})
-        
+        user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+        user.save()
         student_data = StudentTab.objects.create(
             id=request.POST['student_id'],
             name=request.POST['name'],
@@ -45,12 +46,9 @@ def register_page(request):
             secondary_email=request.POST['secondary_email']
         )
         student_data.save()
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
-        print(user)
         Account.objects.create(user=user, student_data=student_data).save()
         return JsonResponse({'status': 'sucess'})
 
 def username_dupcheck(request):
-    
     request.POST['username']
     pass
