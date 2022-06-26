c10bc2431fb10d6a191ceb5db3e2e6a196a51d99
meltingOcean <tmdgur325@gmail.com>
2022-06-26 23:57:33 +0900

    profile views and urls

--- osp/user/views.py
+++ osp/user/views.py
@@ -1,16 +1,47 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from user.models import ScoreTable, StudentTab
+from django.contrib.auth.decorators import login_required
+from repository.models import GithubRepoStats, GithubRepoContributor, GithubRepoCommits, GithubIssues, GithubPulls
+
 
 # Create your views here.
 
+
+
 class ProfileView(TemplateView):
-    template_name = 'profile.html'
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(request, *args, **kwargs)
 
-        return render(request=request, template_name=self.template_name, context=context)
+        student_info = StudentTab.objects.get(id=context['user_id'])
+        student_score = ScoreTable.objects.get(id=context['user_id'], year=2021)
+        data = {}
+        data['info'] = student_info
+        data['score'] = student_score
+        
+        return render(request, 'profile.html', {'data': data})
 
     def get_context_data(self, request, *args, **kwargs):
+        
+        context = super().get_context_data(**kwargs)
+
+        return context
+
+class ProfileEditView(TemplateView):
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(request, *args, **kwargs)
+
+        student_info = StudentTab.objects.get(id=context['user_id'])
+        data = {}
+        data['info'] = student_info
+
+        return render(request, 'profile-edit.html', {'data': data})
+
+    def get_context_data(self, request, *args, **kwargs):
+        
         context = super().get_context_data(**kwargs)
         return context
+
