8cac9f842d113744f348e2be31e8dd8c0655b6c0
Seran Kim <49436143+theran23@users.noreply.github.com>
2022-06-27 13:24:34 +0900

    ..

--- osp/common/urls.py
+++ osp/common/urls.py
@@ -2,6 +2,7 @@ from re import template
 from django.urls import path
 from common import views
 from django.contrib.auth import views as auth_views
+app_name = 'common'
 
 urlpatterns = [
   path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
