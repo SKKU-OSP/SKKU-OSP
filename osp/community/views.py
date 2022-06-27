2700d0fe60ccd7ce7c05cc9faebf7048331b4158
Seran Kim <49436143+theran23@users.noreply.github.com>
2022-06-28 00:17:01 +0900

    .

--- osp/community/views.py
+++ osp/community/views.py
@@ -1,15 +1,44 @@
 from django.shortcuts import render
 from .models import Board
 from django.shortcuts import redirect
+from django.views.generic import TemplateView
+
 # Create your views here.
 
-def main(request):
-    return render(request, 'community/main.html')
-
 def board(request, board_name):
-    try:
-        board = Board.objects.get(name=board_name)
-    except Board.DoesNotExist:
+    board = Board.objects.filter(name=board_name)
+    if len(board) == 0:
         return redirect('/community')
-    print(board)
     return render(request, 'community/board.html', {'board': ''})
+
+
+class TeamView(TemplateView):
+
+    def get(self, request, *args, **kwargs):
+        student_id = self.kwargs.get('student_id')
+
+        context = self.get_context_data(request, *args, **kwargs)
+
+
+        # return render(request=request, template_name=self.template_name, context=context)
+        data = {}
+
+
+        return render(request, 'profile/profile.html', {'data': data})
+
+    # ajax 요청 시 POST로 처리됨.(owned/ contributed repository Tab)
+    # def post(self, request, *args, **kwargs):
+    #     context = self.get_context_data(request, *args, **kwargs)
+    #     # POST 요청 시 예외 처리 안함.
+    #     std = self.POST.get('student_id')
+    #     context['std'] = std
+
+    #     github_id = StudentTab.objects.first(id=std.id).github_id
+
+    #     context['cur_repo_type'] = self.POST.get('cur_repo_type')
+    #     context['repos'] = get_repos(context['cur_repo_type'], github_id)
+
+    def get_context_data(self, request, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
