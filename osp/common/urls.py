faff2513f5d9226b0f66bf1cd6500d6915e06e4e
Seran Kim <49436143+theran23@users.noreply.github.com>
2022-06-27 13:27:13 +0900

    .

--- osp/common/urls.py
+++ osp/common/urls.py
@@ -6,5 +6,5 @@ app_name = 'common'
 urlpatterns = [
   path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
   path('logout/', auth_views.LogoutView.as_view(template_name='common/logout.html'), name='logout'),
-  path('register/', views.register_page, name='logout'),
+  path('register/', views.register_page, name='signup'),
 ]
