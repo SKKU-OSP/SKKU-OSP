ec6e3d73004a252ecbc365c3fbe93cb841311def
Seran Kim <49436143+theran23@users.noreply.github.com>
2022-06-27 13:48:10 +0900

    .

--- osp/tag/templatetags/tag_templatetag.py
+++ osp/tag/templatetags/tag_templatetag.py
@@ -3,18 +3,31 @@ from django import template
 register = template.Library()
 import requests
 from django.utils.safestring import mark_safe
+from tag.models import Tag
 
 @register.simple_tag
 def category_tag(request):
         result = ''
-        host_uri = request.get_host()
-        r = requests.get('http://'+host_uri+'/tag/api')
-        if r.status_code == 200:
-                d = r.json()
-                data = d.get('data')
-                print(data)
-                for tag in data:
-                        print(tag)
-                        result += f'<option value="{tag}">{tag}</option>'
+        ##api 사용
+        # host_uri = request.get_host()
+        # r = requests.get('http://'+host_uri+'/tag/api')
+        # if r.status_code == 200:
+        #         d = r.json()
+        #         data = d.get('data')
+        #         print(data)
+        #         for tag in data:
+        #                 print(tag)
+        #                 result += f'<option value="{tag}">{tag}</option>'
+        tags = Tag.objects.all()
+        type_list = list(tags.values_list("type", flat=True).distinct())
+
+        for t in type_list:
+            result += f'<optgroup label="{t}">'
+            objects = Tag.objects.filter(type=t)
+            name_list = list(objects.values_list("name", flat=True).distinct())
+            name_list.sort()
+            for n in name_list:
+                    result += f'<option value="{n}">{n}</option>'
+            result += '</optgroup>'
 
         return mark_safe(result)
