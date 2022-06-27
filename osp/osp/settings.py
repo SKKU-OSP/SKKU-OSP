71c5ba39972aa5cb71d7a134f3b0ed198cd0570a
meltingOcean <tmdgur325@gmail.com>
2022-06-27 10:02:13 +0900

    mysql moudle

--- osp/osp/settings.py
+++ osp/osp/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import pymysql  
+pymysql.install_as_MySQLdb()
 import os
 
 env_mode = 'ENV_MODE' in os.environ
