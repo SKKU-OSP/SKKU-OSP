569ca2c06309ffe75c214a104f0b5040ae545cd1
tsfo1489 <tsfo1489@gmail.com>
2022-06-28 10:00:12 +0900

    Fix: Delete domain filter in register templatetag

--- osp/tag/templatetags/tag_templatetag.py
+++ osp/tag/templatetags/tag_templatetag.py
@@ -8,19 +8,8 @@ from tag.models import Tag
 @register.simple_tag
 def category_tag(request):
         result = ''
-        ##api 사용
-        # host_uri = request.get_host()
-        # r = requests.get('http://'+host_uri+'/tag/api')
-        # if r.status_code == 200:
-        #         d = r.json()
-        #         data = d.get('data')
-        #         print(data)
-        #         for tag in data:
-        #                 print(tag)
-        #                 result += f'<option value="{tag}">{tag}</option>'
         tags = Tag.objects.all()
         type_list = list(tags.values_list("type", flat=True).distinct())
-        type_list.remove('domain')
 
         for t in type_list:
             result += f'<optgroup label="{t}">'
