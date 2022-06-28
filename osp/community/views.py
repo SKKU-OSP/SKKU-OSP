b218478704ded293a39fd622dd29e1842e2700a2
tsfo1489 <tsfo1489@gmail.com>
2022-06-28 09:36:14 +0900

    Feat: Change community main board view

--- osp/community/views.py
+++ osp/community/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from .models import Board
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+from .models import Board, Article
+from datetime import datetime, timedelta
 # Create your views here.
 
+@login_required
 def main(request):
-    return render(request, 'community/main.html')
+    board_list = []
+    for board in Board.objects.all():
+        week_ago = datetime.now() - timedelta(days=7)
+        article_list = Article.objects.filter(board_id=board, 
+                                              pub_date__range=[
+                                                  week_ago.strftime('%Y-%m-%d'),
+                                                  datetime.now().strftime('%Y-%m-%d')
+                                              ])
+        if len(article_list) > 0:
+            article_list = article_list.order_by('-view_cnt')
+            board.top_article = article_list[:min(3, len(article_list))]
+        else:
+            board.top_article = []
+        board_list.append(board)
+    print(board_list)
+    return render(request, 'community/main.html', {'boards': board_list})
 
 def board(request, board_name):
     try:
