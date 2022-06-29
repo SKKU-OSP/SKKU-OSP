a9912cdd4590be5ca8a60c1fe2ef2b69448197eb
tsfo1489 <tsfo1489@gmail.com>
2022-06-29 13:23:55 +0900

    Feat: Add tag filtering

--- osp/community/admin.py
+++ osp/community/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Board, Article, ArticleLike, ArticleComment, ArticleCommentLike
+from .models import Board, Article, ArticleLike, ArticleComment, ArticleCommentLike, ArticleTag
 
 # Register your models here.
 admin.site.register(Board)
 admin.site.register(Article)
+admin.site.register(ArticleTag)
 admin.site.register(ArticleLike)
 admin.site.register(ArticleComment)
 admin.site.register(ArticleCommentLike)
