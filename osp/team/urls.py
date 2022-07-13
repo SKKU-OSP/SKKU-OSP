a9f6aeb2b5c96b6b69d984f109d14926af691528
tsfo1489 <tsfo1489@gmail.com>
2022-07-13 15:19:08 +0900

    Feat: Add team update modal

--- osp/team/urls.py
+++ osp/team/urls.py
@@ -4,6 +4,7 @@ from team import views
 app_name = 'team'
 
 urlpatterns = [
-    path('api/team-create', views.TeamCreate, name='TeamCreate'),
+    path('api/team-create', views.TeamCreate, name='team-create'),
+    path('api/team-update', views.TeamUpdate, name='team-update'),
     # path('api/member', views.TeamMemberAPI.asView(), name='TeamMember API'),
 ]
