09c860cdadcb95a0d095eb95d831479336e70ac3
tsfo1489 <tsfo1489@gmail.com>
2022-07-14 12:23:09 +0900

    Feat: Add Profile Image to sidebar

--- osp/user/templatetags/user_templatetag.py
+++ osp/user/templatetags/user_templatetag.py
@@ -1,23 +1,23 @@
 from django import template
 from django.utils.safestring import mark_safe
-from user.models import StudentTab
+from user.models import Account, StudentTab
 register = template.Library()
 
 @register.simple_tag
 def github_link(*args):
-        link = "https://github.com/"
-        for ele in args:
-                link += ele
-                link += "/"
-        return link
+    link = "https://github.com/"
+    for ele in args:
+        link += ele
+        link += "/"
+    return link
 
 @register.simple_tag
 def tab_repo_type(request_type, tab_type):
-        if request_type == tab_type:
-                return "active"
-        else:
-                return ""
-        
+    if request_type == tab_type:
+        return "active"
+    else:
+        return ""
+
 @register.simple_tag
 def target_github_id(request):
     result = ''
@@ -28,3 +28,8 @@ def target_github_id(request):
         result += f'<option value="{github_id}">{github_id}</option>'
         
     return mark_safe(result)
+
+@register.filter
+def user_profile_image_url(user):
+    acc = Account.objects.get(user=user)
+    return mark_safe(acc.photo.url)
