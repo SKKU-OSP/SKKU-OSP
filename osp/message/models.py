bf761776f89b4e88627b42043d05511a44a40274
tsfo1489 <tsfo1489@gmail.com>
2022-07-15 11:02:52 +0900

    Feat: Add message model

---
+++ osp/message/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from user.models import Account
+
+# Create your models here.
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    # If sender is null, this message is a notification message
+    sender = models.ForeignKey(Account, models.CASCADE, related_name='sender', null=True, blank=True)
+    receiver = models.ForeignKey(Account, models.CASCADE, related_name='receiver')
+    body = models.CharField(max_length=200)
+    send_date = models.DateTimeField(auto_now_add=True)
+    receiver_read = models.BooleanField()
+    sender_delete = models.BooleanField()
+    receiver_delete = models.BooleanField()
