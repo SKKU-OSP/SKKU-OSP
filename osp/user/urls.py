0aed65ff42428679f86265ad939d7edbfd31a2c4
tsfo1489 <tsfo1489@gmail.com>
2022-08-04 10:29:11 +0900

    Fix: Fix rank page

--- osp/user/urls.py
+++ osp/user/urls.py
@@ -4,7 +4,7 @@ from user import views
 
 app_name = 'user'
 urlpatterns = [
-    # path('<int:student_id>/', views.student_id_to_username, name='profile'),
+    path('<int:student_id>/', views.student_id_to_username, name='profile'),
     path('<username>/', views.ProfileView.as_view(), name='profile'),
     path('<username>/profile-edit/', views.ProfileEditView.as_view(), name='profile-edit'),
     path('<username>/comparestat', views.compare_stat, name='comparestat'),
