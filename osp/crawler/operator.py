efd7809e31ed9118a8af4c90b455fc7f7d2895e6
tsfo1489 <tsfo1489@gmail.com>
2022-08-04 09:27:53 +0900

    Feat: Add score update functions

--- osp/crawler/operator.py
+++ osp/crawler/operator.py
@@ -4,6 +4,9 @@ from datetime import datetime
 
 from osp.settings import CRAWLING_LOG_PATH, BASE_DIR
 from user.models import Account
+from home.updateScore import user_score_update
+from challenge.models import Challenge
+from challenge.views import achievement_check
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import register_events, DjangoJobStore
@@ -15,13 +18,19 @@ def start():
     @scheduler.scheduled_job('cron', hour=0, minute=0, id='crawling')
     def crawl_job():
         github_id_list = Account.objects.all().values_list('student_data__github_id', flat=True)
-        github_id_list = '\n'.join(github_id_list)
+        github_id_list = ','.join(github_id_list)
         log_file = os.path.join(CRAWLING_LOG_PATH, f'{datetime.now().strftime("%y%m%d_crawl.log")}')
         print('Start!')
         subprocess.Popen(
-            f'scrapy crawl github --loglevel=INFO --logfile={log_file}',
+            f'scrapy crawl github -a ids={github_id_list} --loglevel=INFO --logfile={log_file}',
             shell=True,
             cwd=os.path.join(BASE_DIR, 'crawler/Scrapy/SKKU_GitHub'),
         )
         print('End!')
+        challenge_list = Challenge.objects.all()
+        year = datetime.now().year
+        for user in Account.objects.all():
+            for chal in challenge_list:
+                achievement_check(user, chal)
+            user_score_update(user, year)
     scheduler.start()
