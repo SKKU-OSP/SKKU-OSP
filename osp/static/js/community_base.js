87465b0aafa4359f36eef57c942e39b4e386614f
Seran Kim <49436143+theran23@users.noreply.github.com>
2022-08-10 13:28:02 +0900

    Fix: 댓글 textarea로 변경

--- osp/static/js/community_base.js
+++ osp/static/js/community_base.js
@@ -12,26 +12,26 @@ $(function () {
 })
 
 $(document).ready(function(){
-    $('.slick-carousel').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        // autoplay: true,
-
-        autoplay: false,
-        autoplaySpeed: 2000,
-        prevArrow: '<div class="a-left prev slick=prev control-c pointer opacity-hover" style="line-height:140px;padding-right:5px;"><</div>',
-        nextArrow: '<div class="a-left prev slick=prev control-c pointer opacity-hover" style="line-height:140px;padding-left:5px;">></div>',
-        arrows:true,
-        responsive:[
-            {
-                breakpoint:900,
-                settings:{
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    autoplaySpeed: 4000,
-                }
-            },
-
-        ]
-    });
+    // $('.slick-carousel').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     // autoplay: true,
+    //
+    //     autoplay: false,
+    //     autoplaySpeed: 2000,
+    //     prevArrow: '<div class="a-left prev slick=prev control-c pointer opacity-hover" style="line-height:140px;padding-right:5px;"><</div>',
+    //     nextArrow: '<div class="a-left prev slick=prev control-c pointer opacity-hover" style="line-height:140px;padding-left:5px;">></div>',
+    //     arrows:true,
+    //     responsive:[
+    //         {
+    //             breakpoint:900,
+    //             settings:{
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1,
+    //                 autoplaySpeed: 4000,
+    //             }
+    //         },
+    //
+    //     ]
+    // });
 });
