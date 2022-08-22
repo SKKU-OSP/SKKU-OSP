d941bdbf3d0655b80dd44bdfaf5f5508212ff131
tsfo1489 <tsfo1489@gmail.com>
2022-08-22 13:00:40 +0900

    feat: Update Challenge page

--- osp/challenge/models.py
+++ osp/challenge/models.py
@@ -4,12 +4,19 @@ from user.models import Account
 
 # Create your models here.
 class Challenge(models.Model):
+    TIER_CHOICE = (
+        (0, 'UNKNOWN'),
+        (1, 'BRONZE'),
+        (2, 'SILVER'),
+        (3, 'GOLD')
+    )
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     image = models.ImageField(default='img/challenge/default.jpg', upload_to='img/challenge/')
     sql = models.TextField()
     max_progress = models.IntegerField(blank=True, null=True, default=True)
+    tier = models.IntegerField(choices=TIER_CHOICE)
     
 class ChallengeAchieve(models.Model):
     id = models.AutoField(primary_key=True)
