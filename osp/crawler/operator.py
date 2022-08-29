aa2210c151c8db660ed76824b391d29b7f819bc6
tsfo1489 <tsfo1489@gmail.com>
2022-08-29 13:53:54 +0900

    fix: Fix crawler options

--- osp/crawler/operator.py
+++ osp/crawler/operator.py
@@ -17,21 +17,22 @@ def start():
     scheduler=BackgroundScheduler(timezone='Asia/Seoul')
     scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
     register_events(scheduler)
-    @scheduler.scheduled_job('cron', hour=0, minute=1, id='crawling')
+    @scheduler.scheduled_job('cron', hour=13, minute=51, id='crawling')
     def crawl_job():
-        github_id_list = Account.objects.filter(user__is_admin=False).values_list('student_data__github_id', flat=True)
+        github_id_list = Account.objects.filter(user__is_superuser=False).values_list('student_data__github_id', flat=True)
         github_id_list = ','.join(github_id_list)
         log_file = os.path.join(CRAWLING_LOG_PATH, f'{datetime.now().strftime("%y%m%d_crawl.log")}')
         print('Start!')
+        print(f'scrapy crawl github -a ids={github_id_list} --loglevel=INFO --logfile={log_file}')
         subprocess.Popen(
-            f'scrapy crawl github -a ids={github_id_list} --loglevel=INFO --logfile={log_file}',
+            f'scrapy crawl github -a ids="{github_id_list}" --loglevel=INFO --logfile={log_file}',
             shell=True,
             cwd=os.path.join(BASE_DIR, 'crawler/Scrapy/SKKU_GitHub'),
         )
         print('End!')
         challenge_list = Challenge.objects.all()
         year = datetime.now().year
-        for user in Account.objects.all():
+        for user in Account.objects.filter(user__is_superuser=False):
             for chal in challenge_list:
                 achievement_check(user, chal)
             user_score_update(user, year)
