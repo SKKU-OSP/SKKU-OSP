ebc69056fbe33e1205859bb514f8b254a90b6e42
meltingOcean <tmdgur325@gmail.com>
2022-10-03 16:07:56 +0900

    Fix: delete unused form classes

--- osp/user/forms.py
+++ osp/user/forms.py
@@ -1,25 +1,8 @@
 from django.forms import ModelForm
-from .models import Account, AccountInterest, StudentTab
-
-class ProfileInfoUploadForm(ModelForm):
-    class Meta:
-        model = StudentTab
-        fields = ['plural_major', 'personal_email', 'primary_email', 'secondary_email']
-        
+from .models import Account
+    
 class ProfileImgUploadForm(ModelForm):
     class Meta:
         model = Account
         fields = ['photo']
-
-class PortfolioUploadForm(ModelForm):
-    class Meta:
-        model = Account
-        fields = ['portfolio']
-
-class IntroductionUploadForm(ModelForm):
-    class Meta:
-        model = Account
-        fields = ['introduction']
-
-
 
