baf3e1991b90d04017bb765c3ae039d510f9ff76
SeoJeongYeop <sjyswe99@gmail.com>
2022-10-31 18:54:11 +0900

    feat: add check input id and password - check github id by using api - check password check and show valid text

--- osp/common/urls.py
+++ osp/common/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
   path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
   path('logout/', auth_views.LogoutView.as_view(template_name='common/logout.html'), name='logout'),
   path('register/', views.register_page, name='signup'),
-  path('register/check', views.check_user, name='check_user'),
+  path('register/checkuser', views.check_user, name='check_user'),
+  path('register/checkgithub', views.check_github_id, name='check_github_id'),
 ]
