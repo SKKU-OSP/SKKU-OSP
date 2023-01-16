370167f7bb83ce239d600100c3e0a4cc96f0668b
SeoJeongYeop <sjyswe99@gmail.com>
2023-01-16 18:20:48 +0900

    feat: 유저 랭크 페이지에 csv, excel 다운로드 추가 - datatables 의존성 업데이트

--- osp/static/js/user_rank.js
+++ osp/static/js/user_rank.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $.LoadingOverlay("show");
     var last_year = 0;
     $('#scoreTable > tbody > tr > td:nth-child(7)').each(function () {
         last_year = Math.max(Number($(this).html()), last_year);
@@ -8,22 +9,45 @@ $(document).ready(function () {
         language: {
             searchPanes: {
                 clearMessage: '초기화',
-                collapse: {0: '&#61263', _: '&#61263'}
+                collapse: {0: '<i class="bi bi-funnel"></i>', _: '<i class="bi bi-funnel-fill"></i>'}
             }
         },
-        buttons: [{
-            extend: 'searchPanes',
-            config: {
-                layout: 'columns-5',
-                viewTotal: true,
-                orderable: false,
-                columns: [2, 3, 4, 6, 7],
-                preSelect: [{
-                    column: 6,
-                    rows: [String(last_year)]
-                }]
+        lengthChange: false,
+        buttons: {
+            dom: {
+                button: {
+                    className: ''
+                },
+                container: {
+                    className: 'dt-buttons mb-2'
+                },
             },
-        }],
+            buttons:[
+                {
+                    extend: 'searchPanes',
+                    className: 'btn btn-sm btn-light',
+                    config: {
+                        layout: 'columns-5',
+                        viewTotal: true,
+                        orderable: false,
+                        columns: [2, 3, 4, 6, 7],
+                        preSelect: [{
+                            column: 6,
+                            rows: [String(last_year)]
+                        }]
+                    },
+                },
+                {
+                    extend: 'csv',
+                    className: 'btn btn-sm btn-success',
+                    title: 'user_rank'
+                },
+                {
+                    extend: 'excel',
+                    className: 'btn btn-sm btn-success',
+                    title: 'user_rank'
+                },
+            ]},
         columnDefs: [{
             orderable: false,
             targets: [0, 1, 2, 3, 4, 5, 6]
@@ -36,4 +60,5 @@ $(document).ready(function () {
         return this.nodeType === 3;
     }).remove();
     $('#scoreTable_filter > label > input').attr('placeholder', 'Search')
+    $.LoadingOverlay("hide");
 });
