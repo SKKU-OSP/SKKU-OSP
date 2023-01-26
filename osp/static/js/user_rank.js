5e7fef79646292bbb787cb98fdb6705f1e944567
SeoJeongYeop <sjyswe99@gmail.com>
2023-01-26 11:35:21 +0900

    feat: user rank 데이터 테이블에 학번, 이메일 열 추가 - 로딩바 추가하여 UX 개선 - 데이터 테이블 업데이트에 이메일도 포함하도록 코드 추가

--- osp/static/js/user_rank.js
+++ osp/static/js/user_rank.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var last_year = 0;
-    $('#scoreTable > tbody > tr > td:nth-child(7)').each(function () {
+    $('#scoreTable > tbody > tr > td:nth-child(9)').each(function () {
         last_year = Math.max(Number($(this).html()), last_year);
     })
     var table = $('#scoreTable').DataTable({
@@ -32,9 +32,9 @@ $(document).ready(function () {
                         layout: 'columns-5',
                         viewTotal: true,
                         orderable: false,
-                        columns: [2, 3, 4, 6, 7],
+                        columns: [4, 5, 6, 8, 9],
                         preSelect: [{
-                            column: 6,
+                            column: 8,
                             rows: [String(last_year)]
                         }]
                     },
@@ -52,7 +52,7 @@ $(document).ready(function () {
             ]},
         columnDefs: [{
             orderable: false,
-            targets: [0, 1, 2, 3, 4, 5, 6],
+            targets: [3, 4, 5, 6],
             searchPanes: {
                 dtOpts: {
                     select: {
@@ -60,13 +60,19 @@ $(document).ready(function () {
                     }
                 }
             }
-        }, ],
+        }, 
+        {
+            targets:[2, 3],
+            visible: false
+        }],
         order: [
-            [7, 'desc']
+            [0, 'asc']
         ],
     });
     $('#scoreTable_filter > label').contents().filter(function(){
         return this.nodeType === 3;
     }).remove();
-    $('#scoreTable_filter > label > input').attr('placeholder', 'Search')
+    $('#scoreTable_filter > label > input').attr('placeholder', 'Search');
+    $("#loading").css("display", "none");
+    $("#scoreTable").addClass("show");
 });
