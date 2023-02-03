a40bc1600efcc710f3025641632b3656f520eab1
SeoJeongYeop <sjyswe99@gmail.com>
2023-02-03 11:41:59 +0900

    fix: 회원가입에서 이메일이 올바르게 설정되지 않는 오류 수정 - 이메일 정규식 체크 추가 - 이메일 리스트 업데이트 - id 속성 언더스코어 하이픈으로 교체

--- osp/common/views.py
+++ osp/common/views.py
@@ -15,22 +15,37 @@ def register_page(request):
         fail_reason = []
         if len(request.POST.get('username')) < 5:
             fail_reason.append('username은 5자 이상이여야 합니다.')
-        if request.POST['password_check'] != request.POST['password']:
+        if request.POST['password-check'] != request.POST['password']:
             fail_reason.append('password가 일치하지 않습니다.')
-        if not re.match('[0-9]{10}', request.POST['student_id']) :
+        if not re.match('[0-9]{10}', request.POST['student-id']) :
             fail_reason.append('학번 형식이 다릅니다.')
         if len(request.POST['name']) > 20:
             fail_reason.append('이름은 20자를 넘을 수 없습니다.')
         if len(request.POST['dept']) > 45:
             fail_reason.append('학과은 45자를 넘을 수 없습니다.')
-        if len(request.POST['personal_email']) > 100:
+        if len(request.POST['personal-email']) > 100:
             fail_reason.append('이메일 주소는 100자를 넘을 수 없습니다.')
-        if len(request.POST['primary_email']) > 100:
+        if len(request.POST['primary-email']) > 100:
             fail_reason.append('이메일 주소는 100자를 넘을 수 없습니다.')
-        if len(request.POST['secondary_email']) > 100:
+        if len(request.POST['secondary-email']) > 100:
             fail_reason.append('이메일 주소는 100자를 넘을 수 없습니다.')
         if check_duplicate_username(request.POST.get('username')):
-            fail_reason.append('중복된 이름이 있습니다.')
+            fail_reason.append('중복된 이름이 있습니다.\n')
+
+        personal_email=request.POST['personal-email'] + "@" + request.POST['personal-email-domain']
+        primary_email=request.POST['primary-email'] + "@" +  request.POST['primary-email-domain']
+        secondary_email=request.POST['secondary-email'] + "@" +  request.POST['secondary-email-domain']
+        try:
+            if check_email(personal_email):
+                fail_reason.append('연락용 이메일이 형식에 맞지 않습니다. ' + personal_email)
+            if check_email(primary_email):
+                fail_reason.append('GitHub 이메일이 형식에 맞지 않습니다. ' + primary_email)
+            if check_email(secondary_email):
+                fail_reason.append('기타 연동 이메일이 형식에 맞지 않습니다. ' + secondary_email)
+        except:
+            fail_reason.append('이메일이 형식에 맞지 않습니다.')
+
+
         if len(fail_reason) > 0:
             return JsonResponse({'status': 'fail', 'message': fail_reason})
         try:
@@ -38,16 +53,16 @@ def register_page(request):
                 user = User.objects.create_user(username=request.POST.get('username'), password=request.POST['password'])
                 user.save()
                 student_data = StudentTab.objects.create(
-                    id=request.POST['student_id'],
+                    id=request.POST['student-id'],
                     name=request.POST['name'],
                     college=request.POST['college'],
                     dept=request.POST['dept'],
-                    github_id=request.POST['github_id'],
+                    github_id=request.POST['github-id'],
                     absence=request.POST['absence'],
-                    plural_major=request.POST['plural_major'],
-                    personal_email=request.POST['personal_email'],
-                    primary_email=request.POST['primary_email'],
-                    secondary_email=request.POST['secondary_email']
+                    plural_major=request.POST['plural-major'],
+                    personal_email=request.POST['personal-email'],
+                    primary_email=request.POST['primary-email'],
+                    secondary_email=request.POST['secondary-email']
                 )
                 student_data.save()
                 new_account = Account.objects.create(user=user, student_data=student_data)
@@ -89,7 +104,7 @@ def check_user(request):
 def check_github_id(request):
     if request.method == 'POST':
         fail_reason = []
-        github_id = request.POST.get('github_id')
+        github_id = request.POST.get('github-id')
         
         if len(github_id) == 0:
             fail_reason.append('GitHub ID를 입력해주세요.')
@@ -123,4 +138,8 @@ def check_api_github(github_id):
             print("check error github_id", e)
     
     return False
-    
+
+def check_email(email):
+    p = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+    print(email, p.match(email) != None)
+    return p.match(email) == None
