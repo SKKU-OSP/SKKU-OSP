8d9b590139dd714a106afdf4afd041b7298d897d
SeoJeongYeop <sjyswe99@gmail.com>
2023-02-13 10:30:01 +0900

    design: 로그인 관련 페이지에서 검색창 숨기도록 수정

--- osp/common/urls.py
+++ osp/common/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth import views as auth_views
 app_name = 'common'
 
 urlpatterns = [
-  path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
-  path('logout/', auth_views.LogoutView.as_view(template_name='common/logout.html'), name='logout'),
+  path('login/', views.LoginView.as_view(template_name='common/login.html'), name='login'),
+  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
   path('register/', views.register_page, name='signup'),
   path('register/checkuser', views.check_user, name='check_user'),
   path('register/checkgithub', views.check_github_id, name='check_github_id'),
