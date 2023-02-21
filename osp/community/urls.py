bc65460f6f13bb0f2baa46834ad6841a27a016ca
SeoJeongYeop <sjyswe99@gmail.com>
2023-02-21 16:53:35 +0900

    feat: 공지 게시글 쓰기 기능 추가 - 팀장이 팀게시판에서 공지쓰는 기능 추가(일반공지) - admin이 공지게시판에서 공지쓰는 기능 추가(전체공지) - 체크박스로 구분 - 공지게시판용 url 추가

--- osp/community/urls.py
+++ osp/community/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('redirect/', views.redirectView.as_view(), name='RedirectView'),
     path('board/<board_name>/<board_id>/', views.TableBoardView.as_view(), name='Board'),
     path('board/<board_name>/<board_id>/save/', views.ArticleSaveView.as_view(), name='article-save'),
-    path('board/<board_name>/<board_id>/notice-save/', views.ArticleNoticeSaveView.as_view(), name='article-notice-save'),
+    path('board/notice/', views.NoticeView.as_view(), name='notice-board'),
+    path('board/notice-save/', views.ArticleNoticeSaveView.as_view(), name='notice-save'),
 
     path('search/', views.SearchView.as_view(), name='Search'),
 
