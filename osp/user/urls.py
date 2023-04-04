19e95152512ebe0c89ebf1d8c20656c0e6d3e13f
SeoJeongYeop <sjyswe99@gmail.com>
2023-04-04 15:42:06 +0900

    feat: chart data를 ajax를 통해 받아오는 것으로 수정 - 받은 chart data를 render 함수로 차트영역에 그리도록 수정 - 비로그인 시 profile basic 오류수정

--- osp/user/urls.py
+++ osp/user/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
 
     path('<username>/api/consent-write', views.consent_write, name='consent_write'),
     path('<username>/api/consent-open', views.consent_open, name='consent_open'),
+    path('<username>/api/chart-data', views.get_chart_data, name='chart-data'),
 ]
