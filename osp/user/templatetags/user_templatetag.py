f1957f40b46df1a76aaa8ae34874a17df3697ef1
wjieun <wjieun@g.skku.edu>
2023-04-05 14:56:57 +0900

    fix: 활동내역 공개 시에만 기여 평가 비교 대상으로 설정

--- osp/user/templatetags/user_templatetag.py
+++ osp/user/templatetags/user_templatetag.py
@@ -1,7 +1,8 @@
 from django import template
 from django.utils.safestring import mark_safe
-from user.models import Account, StudentTab
+from user.models import Account, StudentTab, AccountPrivacy
 from osp.settings import BASE_DIR
+from django.contrib.auth.models import User
 import os, json
 DATA_DIR = os.path.join(BASE_DIR, "static/data/")
 register = template.Library()
@@ -24,11 +25,12 @@ def tab_repo_type(request_type, tab_type):
 @register.simple_tag
 def target_github_id(request):
     result = ''
-    student = StudentTab.objects.values("github_id").all()
+    account = AccountPrivacy.objects.filter(open_lvl__gt=0).values("account_id").all()
 
-    for st in student:
-        github_id = st['github_id']
-        result += f'<option value="{github_id}">{github_id}</option>'
+    for acc in account:
+        account_id = acc['account_id']
+        username = User.objects.get(id=account_id)
+        result += f'<option value="{username}">{username}</option>'
         
     return mark_safe(result)
 
