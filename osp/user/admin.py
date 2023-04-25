108876db7a05cee6b9f0fd26f803fc3272c2d9bf
SeoJeongYeop <sjyswe99@gmail.com>
2023-04-25 16:07:01 +0900

    feat: account admin 에서 github_id로 검색할 수 있도록 수정

--- osp/user/admin.py
+++ osp/user/admin.py
@@ -3,7 +3,14 @@ from .models import StudentTab, Account, GitHubScoreTable
 # Register your models here.
 
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ('user', 'student_data')
+    search_fields = ['github_id']
+    list_display = ('get_user_username', 'get_student_id', 'github_id')
+    @admin.display(description='user username', ordering='user__username')
+    def get_user_username(self, obj):
+        return obj.user.username
+    @admin.display(description='student_data id', ordering='student_data__id')
+    def get_student_id(self, obj):
+        return obj.student_data_id
 
 class StudentAdmin(admin.ModelAdmin):
     search_fields = ['id', 'name', 'github_id']
