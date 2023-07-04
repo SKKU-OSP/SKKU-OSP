2d268e65347d13c2219b92f9b52dd5c0f7acf09d
SeoJeongYeop <sjyswe99@gmail.com>
2023-07-04 14:59:49 +0900

    feat: 알림 메시지 테이블 추가 - 기존 메시지 테이블에 저장된 알림 메시지를 분리하기 위해 추가

--- osp/message/models.py
+++ osp/message/models.py
@@ -2,14 +2,27 @@ from django.db import models
 
 from user.models import Account
 
-# Create your models here.
+
 class Message(models.Model):
     id = models.AutoField(primary_key=True)
-    # If sender is null, this message is a notification message
-    sender = models.ForeignKey(Account, models.CASCADE, related_name='sender', null=True, blank=True)
-    receiver = models.ForeignKey(Account, models.CASCADE, related_name='receiver')
+    sender = models.ForeignKey(
+        Account, models.CASCADE, related_name='sender', null=True, blank=True)
+    receiver = models.ForeignKey(
+        Account, models.CASCADE, related_name='receiver')
     body = models.TextField()
     send_date = models.DateTimeField(auto_now_add=True)
     receiver_read = models.BooleanField()
     sender_delete = models.BooleanField()
     receiver_delete = models.BooleanField()
+
+
+class Notification(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=20)
+    sender_name = models.CharField(max_length=150)
+    receiver = models.ForeignKey(Account, models.CASCADE)
+    body = models.TextField()
+    send_date = models.DateTimeField(auto_now_add=True)
+    receiver_read = models.BooleanField()
+    receiver_delete = models.BooleanField()
+    route_id = models.IntegerField(null=True, blank=True)
