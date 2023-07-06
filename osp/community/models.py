604ad2583165b2161b17f2e6fb00feba348ebb03
SeoJeongYeop <sjyswe99@gmail.com>
2023-07-06 11:12:48 +0900

    fix: community board에 unique 설정 추가

--- osp/community/models.py
+++ osp/community/models.py
@@ -3,16 +3,19 @@ from user.models import Account
 from tag.models import Tag
 from team.models import Team
 # Create your models here.
+
+
 class Board(models.Model):
-    DEFAULT_BOARDNAME = ['QnA','User','팀 모집','정보','홍보']
+    DEFAULT_BOARDNAME = ['QnA', 'User', '팀 모집', '정보', '홍보']
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     board_type = models.CharField(max_length=10)
     anonymous_writer = models.BooleanField()
     team = models.ForeignKey(Team, models.CASCADE, blank=True, null=True)
-    
+
     def __str__(self) -> str:
         return f'{self.id:03d}:{self.name}({self.board_type})'
+
 
 class Article(models.Model):
     id = models.AutoField(primary_key=True)
@@ -27,13 +30,15 @@ class Article(models.Model):
     writer = models.ForeignKey(Account, models.SET_NULL, blank=True, null=True)
     period_start = models.DateTimeField(blank=True, null=True)
     period_end = models.DateTimeField(blank=True, null=True)
-    
+
     def __str__(self) -> str:
         return f'{self.id:03d}:{self.title}'
+
 
 class ArticleTag(models.Model):
     article = models.ForeignKey(Article, models.CASCADE)
     tag = models.ForeignKey(Tag, models.CASCADE)
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
@@ -45,9 +50,11 @@ class ArticleTag(models.Model):
     def __str__(self) -> str:
         return f'{self.tag.name}: {ellipsis_name(self.article.title)}'
 
+
 class ArticleLike(models.Model):
     article = models.ForeignKey(Article, models.CASCADE)
     account = models.ForeignKey(Account, models.CASCADE)
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
@@ -55,7 +62,7 @@ class ArticleLike(models.Model):
                 name='unique_account_article'
             )
         ]
-    
+
     def __str__(self) -> str:
         return f'{self.account.user}: {ellipsis_name(self.article.title)}'
 
@@ -63,6 +70,7 @@ class ArticleLike(models.Model):
 class ArticleScrap(models.Model):
     article = models.ForeignKey(Article, models.CASCADE)
     account = models.ForeignKey(Account, models.CASCADE)
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
@@ -70,6 +78,7 @@ class ArticleScrap(models.Model):
                 name='unique_account_article_Scrap'
             )
         ]
+
 
 class ArticleComment(models.Model):
     id = models.AutoField(primary_key=True)
@@ -80,13 +89,15 @@ class ArticleComment(models.Model):
     anonymous_writer = models.BooleanField()
     is_deleted = models.BooleanField(default=False)
     writer = models.ForeignKey(Account, models.SET_NULL, blank=True, null=True)
-    
+
     def __str__(self) -> str:
         return f'{self.id:03d}:{self.body}'
+
 
 class ArticleCommentLike(models.Model):
     comment = models.ForeignKey(ArticleComment, models.CASCADE)
     account = models.ForeignKey(Account, models.CASCADE)
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
@@ -94,13 +105,15 @@ class ArticleCommentLike(models.Model):
                 name='unique_account_comment'
             )
         ]
-    
+
     def __str__(self) -> str:
         return f'{self.account.user}: {ellipsis_name(self.comment.body)}'
+
 
 class TeamRecruitArticle(models.Model):
     team = models.ForeignKey(Team, models.CASCADE)
     article = models.ForeignKey(Article, models.CASCADE)
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
@@ -108,6 +121,7 @@ class TeamRecruitArticle(models.Model):
                 name='unique_team_article'
             )
         ]
+
 
 class ArticleImage(models.Model):
     id = models.AutoField(primary_key=True)
@@ -120,6 +134,7 @@ class ArticleImage(models.Model):
 
     def __str__(self) -> str:
         return str(self.image.name)
+
 
 class ArticleFile(models.Model):
     id = models.AutoField(primary_key=True)
@@ -134,7 +149,8 @@ class ArticleFile(models.Model):
     def __str__(self) -> str:
         return str(self.image.name)
 
-def ellipsis_name(name, max_length = 20):
+
+def ellipsis_name(name, max_length=20):
     if len(name) > max_length:
         return name[:max_length] + '...'
     else:
