44ca616f8151a468e46a170ea981c5bdd4d9ea54
SeoJeongYeop <sjyswe99@gmail.com>
2023-07-13 15:40:26 +0900

    feat: challenge 리스트 및 달성 여부 API 추가

--- osp/challenge/models.py
+++ osp/challenge/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from user.models import Account
 
-# Create your models here.
+
 class Challenge(models.Model):
     TIER_CHOICE = (
         (0, 'UNKNOWN'),
@@ -13,11 +13,13 @@ class Challenge(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
-    image = models.ImageField(default='img/challenge/default.jpg', upload_to='img/challenge/')
+    image = models.ImageField(
+        default='img/challenge/default.jpg', upload_to='img/challenge/')
     sql = models.TextField()
     max_progress = models.IntegerField(blank=True, null=True, default=True)
     tier = models.IntegerField(choices=TIER_CHOICE)
-    
+
+
 class ChallengeAchieve(models.Model):
     id = models.AutoField(primary_key=True)
     account = models.ForeignKey(Account, models.CASCADE)
