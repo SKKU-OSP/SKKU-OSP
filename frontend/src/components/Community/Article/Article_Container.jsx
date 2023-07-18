cb8df7b140548003d37f0f0907cbcbae576f96ca
605s <ploki7335@naver.com>
2023-07-18 15:36:51 +0900

    Finish Article

--- frontend/src/components/Community/Article/Article_Container.jsx
+++ frontend/src/components/Community/Article/Article_Container.jsx
@@ -1,17 +1,5 @@
 import Article_Presenter from './Article_Presenter';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
 function Article_Container() {
-  const { article_id } = useParams();
-  const [isExistArticle, setIsExisArticle] = useState(false);
-
-  useEffect(() => {
-    // 존재하는 게시판인지 확인
-    setIsExisArticle(true);
-  });
-
-  return <Article_Presenter isExistArticle={isExistArticle} article_id={article_id} />;
+  return <Article_Presenter />;
 }
-
 export default Article_Container;
