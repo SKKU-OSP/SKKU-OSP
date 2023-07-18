1d603fe4608dce2d1d0daf8b1b68954488189517
605s <ploki7335@naver.com>
2023-07-18 15:24:55 +0900

    Edit Article

--- frontend/src/components/Community/Article/TinyArticle/Comment_Item.jsx
+++ frontend/src/components/Community/Article/TinyArticle/Comment_Item.jsx
@@ -1,11 +1,22 @@
 import '../../Community.css';
 import '../Article.css';
+import '../base.css';
+import { FaRegThumbsUp, FaThumbsUp } from 'react-icons/fa';
 
 function Comment_Item(props) {
   const comment = props.comments;
   const request = { user: { username: 'hoya', id: 1234, is_authenticated: false, is_superuser: false } };
   const user = { id: 1234 };
   const is_comment_like = false;
+  let pub_date1, pub_date2;
+  const dateObject = new Date(comment.pub_date);
+  pub_date1 = dateObject.toISOString().slice(0, 10);
+  const hours = dateObject.getHours();
+  const minutes = dateObject.getMinutes();
+  const period = hours >= 12 ? 'p.m.' : 'a.m.';
+  const formattedHours = hours % 12 || 12;
+  pub_date2 = `${formattedHours}:${minutes.toString().padStart(2, '0')} ${period}`;
+
   return (
     <>
       <div className="comment-item-left">
@@ -26,7 +37,6 @@ function Comment_Item(props) {
                 {!comment.writer.user.is_superuser && (
                   <li>
                     <a className="dropdown-item" href={`/user/${comment.writer}`}>
-                      {' '}
                       {/*comment.writer.user.username*/}
                       프로필
                     </a>
@@ -34,7 +44,6 @@ function Comment_Item(props) {
                 )}
                 <li>
                   <a className="dropdown-item" onclick={`msgModalOpen(${comment.writer})`}>
-                    {' '}
                     {/*comment.writer.user.id*/}
                     메시지
                   </a>
@@ -49,23 +58,27 @@ function Comment_Item(props) {
             <div className="comment-item-delete">
               <span onclick={`comment.delete(${comment.id});`}>삭제</span>
             </div>
-          )}{' '}
+          )}
           {/*request.user == comment.writer.user*/}
         </div>
         <div className="comment-item-body">{comment.body}</div>
       </div>
       <div className="comment-item-right">
         <div className="comment-item-datetime">
-          <div>{comment.pub_date}</div>
-          <div>{comment.pub_date}</div>
+          <div>{pub_date1}</div>
+          <div>{pub_date2}</div>
         </div>
         <div className="comment-item-mod">
           {/* {% is_comment_like comment request.user as is_comment_like %} */}
           <div className="comment-item-like" onclick={`CommentLike(${comment.id}, ${user.id})`}>
             {is_comment_like ? (
-              <span className="material-icons">thumb_up</span>
+              <span className="material-icons">
+                <FaThumbsUp />
+              </span>
             ) : (
-              <span className="material-icons-outlined">thumb_up</span>
+              <span className="material-icons-outlined">
+                <FaRegThumbsUp />
+              </span>
             )}
           </div>
           <div className="comment-item-like-cnt">{comment.like}</div>
