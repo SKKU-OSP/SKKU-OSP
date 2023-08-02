806083dd6a0ef021b7db34ff04fc0944eab9993f
wjieun <wjieun@g.skku.edu>
2023-08-02 19:01:01 +0900

    sidebar, community navbar

--- frontend/src/components/Community/SideBar/SideBar_Container.jsx
+++ frontend/src/components/Community/SideBar/SideBar_Container.jsx
@@ -1,5 +1,35 @@
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SideBar_Presenter from './SideBar_Presenter';
+import AuthContext from '../../../utils/auth-context';
 
 export default function SideBar_Container() {
-  return <SideBar_Presenter />;
+  const { username, githubUsername } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const onMyProfile = () => {
+    navigate(`/user/${username}`);
+  };
+
+  const onMyGithub = () => {
+    window.open(`https://www.github.com/${githubUsername}`);
+  };
+
+  const onLogin = () => {
+    navigate(`/accounts/login`);
+  };
+
+  useEffect(() => {
+    console.log('username:', username);
+  });
+
+  return (
+    <SideBar_Presenter
+      username={username}
+      githubUsername={githubUsername}
+      onMyProfile={onMyProfile}
+      onMyGithub={onMyGithub}
+      onLogin={onLogin}
+    />
+  );
 }
