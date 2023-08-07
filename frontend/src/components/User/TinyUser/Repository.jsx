bc86476d770fb969aaf3919e3f9c2abc440f5d09
605s <ploki7335@naver.com>
2023-08-07 15:43:01 +0900

    feat. Repository UI 완성

--- frontend/src/components/User/TinyUser/Repository.jsx
+++ frontend/src/components/User/TinyUser/Repository.jsx
@@ -1,15 +1,28 @@
 import '../User.css';
+import {
+  BsStar,
+  BsEyeFill,
+  BsCheckLg,
+  BsCheckCircle,
+  BsRecordCircle,
+  BsBox,
+  BsCardHeading,
+  BsBook,
+  BsKey,
+  BsCheckCircleFill
+} from 'react-icons/bs';
+import { PiGitForkBold, PiGitPullRequestBold } from 'react-icons/pi';
 
 function Repository() {
   const guideline = [
     {
-      owner_id: 1,
-      repo_name: 'dasd',
+      owner_id: 'SeoJeongYeop',
+      repo_name: 'SeoJeongYeop',
       create_date: '1231',
       update_date: '1231',
       committer_date: '23123',
-      proj_short_desc: '231',
-      license: '123',
+      proj_short_desc: 'none',
+      license: 'none',
       star_count: 312,
       watcher_count: 123,
       fork_count: 123,
@@ -18,10 +31,48 @@ function Repository() {
       close_issue_count: 123,
       open_issue_count: 123,
       pr_count: 123,
-      readme: 0
+      readme: 1
+    },
+    {
+      owner_id: 'SeoJeongYeop',
+      repo_name: 'SeoJeongYeop',
+      create_date: '1231',
+      update_date: '1231',
+      committer_date: '23123',
+      proj_short_desc: 'none',
+      license: 'none',
+      star_count: 312,
+      watcher_count: 123,
+      fork_count: 123,
+      commit_count: 123,
+      dependency_count: 123,
+      close_issue_count: 123,
+      open_issue_count: 123,
+      pr_count: 123,
+      readme: 1
+    },
+    {
+      owner_id: 'SeoJeongYeop',
+      repo_name: 'SeoJeongYeop',
+      create_date: '1231',
+      update_date: '1231',
+      committer_date: '23123',
+      proj_short_desc: 'none',
+      license: 'none',
+      star_count: 312,
+      watcher_count: 123,
+      fork_count: 123,
+      commit_count: 123,
+      dependency_count: 123,
+      close_issue_count: 123,
+      open_issue_count: 123,
+      pr_count: 123,
+      readme: 1
     }
   ];
-  let commitDate, createDate, updateDate;
+  let commitDate = '2023-06-13 14:59',
+    createDate = '2023-03-10 07:47',
+    updateDate = '2023-03-10 07:47';
   {
     /* if (guideline !== null) {
     let dateObject, hours, minutes, period, formattedHours;
@@ -58,7 +109,7 @@ function Repository() {
       {guideline.length > 0 ? (
         guideline.map((repo, index) => (
           <>
-            <div class="card d-flex flex-row mx-4 mb-3">
+            <div class="card guideline-check mx-4 mb-3">
               <div class="card-half">
                 <div class="card-body">
                   <div class="card-title d-flex justify-content-between">
@@ -77,82 +128,39 @@ function Repository() {
                       <span class="weak-text">{commitDate}</span>
                     </div>
                   </div>
-                  {repo.proj_short_desc === 'none' ? (
-                    <p class="weak-text">프로젝트 설명이 없습니다.</p>
-                  ) : (
-                    <p>{repo.proj_short_desc}</p>
-                  )}
-
+                  <div class="repo-desc">
+                    {repo.proj_short_desc === 'none' ? (
+                      <p class="weak-text">프로젝트 설명이 없습니다.</p>
+                    ) : (
+                      <p className="weak-text-lg">{repo.proj_short_desc}</p>
+                    )}
+                  </div>
                   <div class="repo-info">
                     <span>
-                      <i class="bi bi-star"></i> Stars {repo.star_count}
+                      <BsStar /> Stars {repo.star_count}
                     </span>
                     <span>
-                      <i class="bi bi-eye-fill"></i> Watchers {repo.watcher_count}
+                      <BsEyeFill /> Watchers {repo.watcher_count}
                     </span>
                     <span>
-                      <svg
-                        aria-hidden="true"
-                        width="14"
-                        height="14"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        data-view-component="true"
-                        class="octicon octicon-repo-forked mr-2"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                        ></path>
-                      </svg>{' '}
-                      Forks {repo.fork_count}
+                      <PiGitForkBold /> Forks {repo.fork_count}
                     </span>
                     <span>
-                      {' '}
-                      <i class="bi bi-check-lg"></i> Commits {repo.commit_count}
+                      <BsCheckLg /> Commits {repo.commit_count}
                     </span>
                     <span>
-                      <svg
-                        aria-hidden="true"
-                        width="14"
-                        height="14"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        data-view-component="true"
-                        class="octicon octicon-git-pull-request UnderlineNav-octicon d-none d-sm-inline"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M7.177 3.073L9.573.677A.25.25 0 0110 .854v4.792a.25.25 0 01-.427.177L7.177 3.427a.25.25 0 010-.354zM3.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122v5.256a2.251 2.251 0 11-1.5 0V5.372A2.25 2.25 0 011.5 3.25zM11 2.5h-1V4h1a1 1 0 011 1v5.628a2.251 2.251 0 101.5 0V5A2.5 2.5 0 0011 2.5zm1 10.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0zM3.75 12a.75.75 0 100 1.5.75.75 0 000-1.5z"
-                        ></path>
-                      </svg>{' '}
+                      <PiGitPullRequestBold />
                       Pull Requests {repo.pr_count}
                     </span>
                     <span>
-                      <svg
-                        aria-hidden="true"
-                        width="14"
-                        height="14"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        data-view-component="true"
-                        class="octicon octicon-issue-opened"
-                      >
-                        <path d="M8 9.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-                        <path
-                          fill-rule="evenodd"
-                          d="M8 0a8 8 0 100 16A8 8 0 008 0zM1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0z"
-                        ></path>
-                      </svg>{' '}
+                      <BsRecordCircle />
                       Open Issues {repo.open_issue_count}
                     </span>
                     <span>
-                      {' '}
-                      <i class="bi bi-check-circle"></i> Closed Issues {repo.close_issue_count}
+                      <BsCheckCircle /> Closed Issues {repo.close_issue_count}
                     </span>
                     <span>
-                      {' '}
-                      <i class="bi bi-box"></i> Dependencies {repo.dependency_count}
+                      <BsBox /> Dependencies {repo.dependency_count}
                     </span>
                   </div>
                   <div class="card-title-block text-right">
@@ -163,9 +171,9 @@ function Repository() {
               </div>
               <div class="card-half">
                 <div class="card-body d-flex flex-column justify-content-between h-100">
-                  <div>
-                    <div>
-                      <i class="bi bi-card-heading"></i>
+                  <div class="repo-git">
+                    <div class="repo-git-info">
+                      <BsCardHeading />
                       {repo.proj_short_desc === 'none' ? (
                         <>
                           <span class="weak-text-lg">리포지토리 Description 없음</span>
@@ -178,8 +186,8 @@ function Repository() {
                         </>
                       )}
                     </div>
-                    <div>
-                      <i class="bi bi-book"></i>
+                    <div class="repo-git-info">
+                      <BsBook />
                       {repo.readme === 0 ? (
                         <>
                           <span class="weak-text-lg">README 파일 없음</span>
@@ -192,8 +200,8 @@ function Repository() {
                         </>
                       )}
                     </div>
-                    <div>
-                      <i class="bi bi-key"></i>
+                    <div class="repo-git-info">
+                      <BsKey />
                       {repo.license === 'none' ? (
                         <>
                           <span class="weak-text-lg">License 파일 없음</span>
@@ -213,7 +221,7 @@ function Repository() {
                         <li class="guideline-step-item unfinished-item">
                           <span class="progress-check"></span>
                           <span class="progress-label">
-                            <i class="bi bi-card-heading"></i>
+                            <BsCardHeading />
                             Short desc
                           </span>
                         </li>
@@ -221,7 +229,7 @@ function Repository() {
                         <li class="guideline-step-item">
                           <span class="progress-check"></span>
                           <span class="progress-label">
-                            <i class="bi bi-card-heading"></i>
+                            <BsCardHeading />
                             Short desc
                           </span>
                         </li>
@@ -231,7 +239,7 @@ function Repository() {
                         <li class="guideline-step-item unfinished-item">
                           <span class="progress-check"></span>
                           <span class="progress-label">
-                            <i class="bi bi-book"></i>
+                            <BsBook />
                             README
                           </span>
                         </li>
@@ -239,7 +247,7 @@ function Repository() {
                         <li class="guideline-step-item">
                           <span class="progress-check"></span>
                           <span class="progress-label">
-                            <i class="bi bi-book"></i>
+                            <BsBook />
                             README
                           </span>
                         </li>
@@ -249,7 +257,7 @@ function Repository() {
                         <li class="guideline-step-item unfinished-item">
                           <span class="progress-check"></span>
                           <span class="progress-label">
-                            <i class="bi bi-key"></i>
+                            <BsKey />
                             license
                           </span>
                         </li>
@@ -257,7 +265,7 @@ function Repository() {
                         <li class="guideline-step-item">
                           <span class="progress-check"></span>
                           <span class="progress-label">
-                            <i class="bi bi-key"></i>
+                            <BsKey />
                             license
                           </span>
                         </li>
