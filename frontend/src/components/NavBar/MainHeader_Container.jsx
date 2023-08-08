7e5faae445c12c8931da07e21de5e2da8e56f577
wjieun <wjieun@g.skku.edu>
2023-08-08 09:50:02 +0900

    main header

--- frontend/src/components/NavBar/MainHeader_Container.jsx
+++ frontend/src/components/NavBar/MainHeader_Container.jsx
@@ -1,9 +1,11 @@
 import MainHeader_Presenter from './MainHeader_Presenter';
 import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../utils/auth-context';
 
 export default function MainHeader_Container(props) {
   const [isToggled, setIsToggled] = useState(false);
+  const navigate = useNavigate();
 
   function onClickToggle() {
     if (isToggled) {
@@ -17,5 +19,9 @@ export default function MainHeader_Container(props) {
   const [isAuth] = useState(true);
   const { username } = useContext(AuthContext);
 
-  return <MainHeader_Presenter username={username} isToggled={isToggled} onClickToggle={onClickToggle} />;
+  const onLogin = () => {
+    navigate(`/accounts/login`);
+  };
+
+  return <MainHeader_Presenter username={username} isToggled={isToggled} onClickToggle={onClickToggle} onLogin={onLogin} />;
 }
