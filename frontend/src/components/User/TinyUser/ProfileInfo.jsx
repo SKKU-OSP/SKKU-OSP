73e250bf7791608a2eddeb500ae5f0a0550bd439
605s <ploki7335@naver.com>
2023-08-09 14:55:40 +0900

    feat. 가이드라인 Repository 백엔드 연결

--- frontend/src/components/User/TinyUser/ProfileInfo.jsx
+++ frontend/src/components/User/TinyUser/ProfileInfo.jsx
@@ -10,6 +10,7 @@ import { useParams } from 'react-router-dom';
 function ProfileInfo() {
   const { user_id } = useParams();
   const [userInfo, setUserInfo] = useState();
+  const [error_occur, setError] = useState(false);
   const server_url = import.meta.env.VITE_SERVER_URL;
   const url = server_url + '/user/api/profile-intro/' + user_id;
 
@@ -22,38 +23,46 @@ function ProfileInfo() {
           console.log('Get', res);
           setUserInfo(res.data.account);
         }
-      } catch (error) {}
+      } catch (error) {
+        setError(true);
+      }
     };
     getProfileInfo();
   }, []);
   return (
     <>
-      {userInfo ? (
-        <div className="d-flex flex-row profile_info">
-          <div className="d-flex align-items-center justify-content-center info_left">
-            <div className="info_img" style={{ backgroundImage: `url(${userInfo.photo})` }}></div>
-          </div>
-          <div className="d-flex flex-column info_right">
-            <div className="d-flex flex-row justify-content-end info_button">
-              <button className="info_btn-1">
-                <span className="info_btn-1-text">대시보드</span>
-              </button>
-              <button className="info_btn-2">
-                <span className="info_btn-2-text">프로필 수정</span>
-              </button>
-            </div>
-            <div className="info_username">
-              <span className="username">{userInfo.user.username}</span>
-            </div>
-            <div className="d-flex flex-row info_github">
-              <BsGithub />
-              <span className="github_username">{userInfo.github_id}</span>
-            </div>
-            <span className="info_introduction">{userInfo.introduction}</span>
-          </div>
-        </div>
+      {error_occur ? (
+        <></>
       ) : (
-        <Spinner animation="border" style={{ position: 'absolute', top: '50%', left: '50%' }} />
+        <>
+          {userInfo ? (
+            <div className="d-flex flex-row profile_info">
+              <div className="d-flex align-items-center justify-content-center info_left">
+                <div className="info_img" style={{ backgroundImage: `url(${userInfo.photo})` }}></div>
+              </div>
+              <div className="d-flex flex-column info_right">
+                <div className="d-flex flex-row justify-content-end info_button">
+                  <button className="info_btn-1">
+                    <span className="info_btn-1-text">대시보드</span>
+                  </button>
+                  <button className="info_btn-2">
+                    <span className="info_btn-2-text">프로필 수정</span>
+                  </button>
+                </div>
+                <div className="info_username">
+                  <span className="username">{userInfo.user.username}</span>
+                </div>
+                <div className="d-flex flex-row info_github">
+                  <BsGithub />
+                  <span className="github_username">{userInfo.github_id}</span>
+                </div>
+                <span className="info_introduction">{userInfo.introduction}</span>
+              </div>
+            </div>
+          ) : (
+            <Spinner animation="border" style={{ position: 'absolute', top: '50%', left: '50%' }} />
+          )}
+        </>
       )}
     </>
   );
