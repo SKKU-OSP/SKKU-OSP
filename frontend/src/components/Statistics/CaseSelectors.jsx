e2783176eb9c2d858ee4e48b114e1b346d1fa2ab
SeoJeongYeop <sjyswe99@gmail.com>
2023-08-14 17:32:16 +0900

    feat: 연도별 평균 통계 차트 렌더링 추가

--- frontend/src/components/Statistics/CaseSelectors.jsx
+++ frontend/src/components/Statistics/CaseSelectors.jsx
@@ -1,26 +1,39 @@
+import { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 
-function CaseSelectors() {
+function CaseSelectors(props) {
+  const [caseNum, setCaseNum] = useState(0);
+
+  const hadleAbsenceSwitch = (e) => {
+    setCaseNum((prev) => (e.target.checked ? prev - 2 : prev + 2));
+  };
+
+  const hadleMajorSwitch = (e) => {
+    setCaseNum((prev) => (e.target.checked ? prev - 1 : prev + 1));
+  };
+
+  useEffect(() => {
+    props.onSetCaseNum(caseNum);
+  }, [props, caseNum]);
+
+  const absenceLabel = caseNum >= 2 ? '미포함' : '포함';
+  const majorLabel = caseNum % 2 ? '미포함' : '포함';
   return (
     <Form className="d-flex text-lg">
-      <Form.Check type="switch" id="absence-switch" label="휴학생 포함" checked />
-      <Form.Check type="switch" id="major-switch" label="복수전공 포함" checked />
-      {/* <div className="form-check form-switch">
-        <input className="form-check-input" id="absenceSwitch" type="checkbox" role="switch" checked="checked" />
-        <label className="form-check-label" htmlFor="absenceSwitch">
-          <span className="bold">휴학생 </span>
-          <span className="switch-toggle-absence none">미포함 </span>
-          <span className="switch-toggle-absence bold">포함 </span>
-        </label>
-      </div>
-      <div className="form-check form-switch">
-        <input className="form-check-input" id="majorSwitch" type="checkbox" role="switch" checked="checked" />
-        <label className="form-check-label" htmlFor="majorSwitch">
-          <span className="bold">복수전공 </span>
-          <span className="switch-toggle-major none">미포함 </span>
-          <span className="switch-toggle-major bold">포함 </span>
-        </label>
-      </div> */}
+      <Form.Check
+        type="switch"
+        label={`휴학생 ${absenceLabel}`}
+        onClick={hadleAbsenceSwitch}
+        className="flex-switch"
+        defaultChecked
+      />
+      <Form.Check
+        type="switch"
+        label={`복수전공 ${majorLabel}`}
+        onClick={hadleMajorSwitch}
+        className="flex-switch"
+        defaultChecked
+      />
     </Form>
   );
 }
