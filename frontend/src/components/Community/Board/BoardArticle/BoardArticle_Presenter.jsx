9cfce6cd12620d520d675a5df0437022b4be9207
wjieun <wjieun@g.skku.edu>
2023-08-17 10:55:35 +0900

    board article - writer

--- frontend/src/components/Community/Board/BoardArticle/BoardArticle_Presenter.jsx
+++ frontend/src/components/Community/Board/BoardArticle/BoardArticle_Presenter.jsx
@@ -5,10 +5,13 @@ export default function BoardArticle_Presenter(props) {
   const { article, onArticle, onWriter } = props;
 
   return <div className="board-article">
-    <h6>{ article.anonymous_writer ?
-    <span className='board-article-writer' onClick={onWriter}>{article.writer.user.username}</span> :
-    <span>익명</span>
-    } · {article.pub_date.substring(0, 10)}</h6>
+    <h6>{ article.writer ? (
+    article.anonymous_writer ?
+    <span>익명</span> :
+    <span className='board-article-writer' onClick={onWriter}>{article.writer.user.username}</span>
+    ) : (
+      <span>탈퇴한 이용자</span>
+    )} · {article.pub_date.substring(0, 10)}</h6>
     <h4 className='board-article-title' onClick={onArticle}>{article.title}</h4>
     <div>
       <h6 className='inline'>#hashtag</h6>
