24c8f52453ec9741d5259cb0d05f60f1a7fbcbc4
wjieun <wjieun@g.skku.edu>
2023-08-17 11:49:15 +0900

    board article - pub_date

--- frontend/src/components/Community/Board/BoardArticle/BoardArticle_Container.jsx
+++ frontend/src/components/Community/Board/BoardArticle/BoardArticle_Container.jsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 export default function BoardArticle_Container(props) {
   const { article } = props;
+  const [pubDate, setPubDate] = useState('');
   const navigate = useNavigate();
 
   const onArticle = () => {
@@ -14,8 +15,27 @@ export default function BoardArticle_Container(props) {
     navigate(`/user/${article.writer.user.username}`)
   };
 
+  const getDate = (date) => {
+    const now = new Date();
+    const article_date = new Date(date);
+    const delta = now.getTime() - article_date.getTime();
+    
+    if (delta / (60 * 1000) < 1) {
+      setPubDate('방금');
+    } else if (delta / (60 * 1000) < 60) {
+      setPubDate(string(delta / (60 * 1000)) + '분 전');
+    } else if (delta / (60 * 60 * 1000) < 24) {
+      setPubDate(string(delta / (60 * 60 * 1000)) + '시간 전');
+    } else if (delta / (24 * 60 * 60 * 1000) < 31) {
+      setPubDate((delta / (24 * 60 * 60 * 1000)).toFixed() + '일 전');
+    } else {
+      setPubDate(date.substring(0, 10));
+    }
+  };
+
   useEffect(() => {
+    getDate(article.pub_date);
   });
 
-  return <BoardArticle_Presenter article={article} onArticle={onArticle} onWriter={onWriter} />;
+  return <BoardArticle_Presenter article={article} pubDate={pubDate} onArticle={onArticle} onWriter={onWriter} />;
 }
