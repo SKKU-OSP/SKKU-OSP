93f5b350a694ab3b440e14278dfbce846c21d528
SeoJeongYeop <sjyswe99@gmail.com>
2023-08-22 17:54:12 +0900

    feat: user rank API 추가

--- osp/rank/views.py
+++ osp/rank/views.py
@@ -1,10 +1,81 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from django.http import JsonResponse
 from django.shortcuts import render
-from user.models import GitHubScoreTable, StudentTab
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from repository.models import GithubRepoStats, GithubRepoContributor, GithubRepoCommits, GithubIssues, GithubPulls
+from user.models import GitHubScoreTable, StudentTab
+from user.serializers import GithubScoreTableSerializer
 
-# Create your views here.
+import logging
+
+
+class UserRanking(APIView):
+    def get_validation(self, request):
+        status = 'fail'
+        errors = {}
+
+        user = request.user
+
+        try:
+            if not user.is_superuser:
+                errors["user_is_not_superuser"] = "어드민계정만 접근할 수 있습니다."
+            else:
+                user = User.objects.get(id=user.id)
+                status = 'success'
+        except User.DoesNotExist:
+            logging.exception(f'UserRanking user_not_found: {e}')
+            errors["user_not_found"] = "해당 유저가 존재하지 않습니다."
+        except Exception as e:
+            logging.exception(f'UserRanking undefined_exception: {e}')
+            errors["undefined_exception"] = "Validation 과정에서 정의되지않은 exception이 발생하였습니다."
+
+        return status, errors
+
+    def get(self, request):
+        res = {"status": "fail", "errors": None, "data": None}
+        data = {}
+        status, errors = self.get_validation(request)
+        if status == 'fail':
+            res['errors'] = errors
+            return Response(res)
+
+        distinct_years = list(GitHubScoreTable.objects.values_list(
+            'year', flat=True).distinct())
+
+        target_year = request.GET.get('year', None)
+        if target_year:
+            score_table = GitHubScoreTable.objects.filter(year=target_year)
+        else:
+            score_table = GitHubScoreTable.objects.all()
+
+        if not score_table.exists():
+            errors["data_not_found"] = "데이터가 존재하지 않습니다."
+            res['status'] = 'fail'
+            res['errors'] = errors
+            return Response(res)
+        score_table_data = GithubScoreTableSerializer(
+            score_table, many=True).data
+        sorted_score_by_year = sorted(
+            score_table_data, key=lambda x: x['score'], reverse=True)
+
+        # 랭킹 부여
+        rank = 1
+        for idx, row in enumerate(sorted_score_by_year):
+            if idx > 0 and sorted_score_by_year[idx]['score'] != sorted_score_by_year[idx - 1]['score']:
+                rank += 1
+            row['rank'] = rank
+
+        data['score_table'] = score_table_data
+        data['years'] = distinct_years
+        res['data'] = data
+        res['status'] = status
+
+        return Response(res)
+
+
 @login_required
 def user_rank(request):
     # 쿼리스트링으로 연도 받음
@@ -17,11 +88,12 @@ def user_rank(request):
             score_by_year[student.year] = []
         score_by_year[student.year].append(student)
     select_year = "ALL"
-    
+
     # 전달받은 연도의 유저 테이블 데이터를 반환
     def sort_year_student(year):
         year_student_list = []
-        sorted_score_by_year = sorted(score_by_year[year], key=lambda x: x.total_score, reverse=True)
+        sorted_score_by_year = sorted(
+            score_by_year[year], key=lambda x: x.total_score, reverse=True)
         rank = 1
         for idx, row in enumerate(sorted_score_by_year):
             if idx > 0 and sorted_score_by_year[idx].total_score != sorted_score_by_year[idx - 1].total_score:
@@ -41,7 +113,8 @@ def user_rank(request):
         for year in score_by_year:
             student_list += sort_year_student(year)
 
-    return render(request, 'rank/user_rank.html', {'data': student_list, 'select_year':select_year, 'year_list': score_by_year.keys()})
+    return render(request, 'rank/user_rank.html', {'data': student_list, 'select_year': select_year, 'year_list': score_by_year.keys()})
+
 
 @login_required
 def repo_rank(request):
@@ -55,17 +128,19 @@ def repo_rank(request):
         repo_link = repo.github_id + '/' + repo.repo_name
         if repo_link in student_contributor:
             repo.student_contributor = student_contributor[repo_link]
-            if (repo.stargazers_count is not None and repo.forks_count is not None 
-                and repo.commits_count is not None and repo.prs_count is not None):
+            if (repo.stargazers_count is not None and repo.forks_count is not None
+                    and repo.commits_count is not None and repo.prs_count is not None):
                 repo_list.append(repo)
     return render(request, 'rank/repo_rank.html', {'data': repo_list})
+
 
 def repo_api(request):
     owner_id = request.GET['github_id']
     repo_name = request.GET['repo_name']
     result = []
     for student in GithubRepoContributor.objects.filter(owner_id=owner_id).filter(repo_name=repo_name):
-        student_profile = list(StudentTab.objects.filter(github_id=student.github_id))
+        student_profile = list(
+            StudentTab.objects.filter(github_id=student.github_id))
         if len(student_profile) != 1:
             continue
         commmit_cnt = len(GithubRepoCommits.objects.filter(
