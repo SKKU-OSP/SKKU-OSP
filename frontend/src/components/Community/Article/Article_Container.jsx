b8c9a3801ac8358a0692f0cc945968cf45bd3e63
605s <ploki7335@naver.com>
2023-08-23 16:20:00 +0900

    feat: Article 페이지 수정

--- frontend/src/components/Community/Article/Article_Container.jsx
+++ frontend/src/components/Community/Article/Article_Container.jsx
@@ -1,5 +1,57 @@
 import Article_Presenter from './Article_Presenter';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import AuthContext from '../../../utils/auth-context';
+import { useContext } from 'react';
+import { getAuthConfig } from '../../../utils/auth';
+import Spinner from 'react-bootstrap/Spinner';
+
 function Article_Container() {
-  return <Article_Presenter />;
+  const [article, setArticle] = useState();
+  const [tags, setTags] = useState();
+  const [comments, setComments] = useState();
+  const [board, setBoard] = useState();
+  const [error_occur, setError] = useState(false);
+  const { article_id } = useParams();
+  const authCtx = useContext(AuthContext);
+
+  const server_url = import.meta.env.VITE_SERVER_URL;
+  const url = server_url + '/community/api/article/' + article_id;
+
+  useEffect(() => {
+    const getArticle = async () => {
+      try {
+        const response = await axios.get(url, getAuthConfig());
+        const res = response.data;
+        console.log(res);
+        if (res.status === 'success') {
+          console.log('GET', res.data, res.message);
+          setArticle(res.data.article);
+          setTags(res.data.tags);
+          setComments(res.data.comments);
+          setBoard(res.data.board);
+        }
+      } catch (error) {
+        console.log(error);
+        setError(true);
+      }
+    };
+    getArticle();
+  }, []);
+
+  return (
+    <>
+      {article && tags && comments && board ? (
+        error_occur ? (
+          <>잘못된 페이지입니다.</>
+        ) : (
+          <Article_Presenter article={article} tags={tags} comments={comments} board={board} />
+        )
+      ) : (
+        <Spinner animation="border" style={{ position: 'absolute', top: '50%', left: '50%' }} />
+      )}
+    </>
+  );
 }
 export default Article_Container;
--- frontend/src/components/Community/Article/Article_Presenter.jsx
+++ frontend/src/components/Community/Article/Article_Presenter.jsx
@@ -1,81 +1,44 @@
 import '../Community.css';
 import './Article.css';
 import ContentView from './TinyArticle/ContentView';
-import ContentEdit from './TinyArticle/ContentEdit';
 import Comment from './TinyArticle/Comment';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import AuthContext from '../../../utils/auth-context';
-import { useContext } from 'react';
-import { getAuthConfig } from '../../../utils/auth';
+import Spinner from 'react-bootstrap/Spinner';
 
-function Article_Presenter() {
+function Article_Presenter(props) {
   const need_login = false;
   const need_member = false;
   const type = 'view';
   // 아직 API로 필요한 것들
-
-  const [article, setArticle] = useState(null);
-  const [tags, setTags] = useState([]);
-  const [comments, setComments] = useState([]);
-  const [board, setBoard] = useState(null);
-  const [error_occur, setError] = useState(false);
-  const authCtx = useContext(AuthContext);
-  const { article_id } = useParams();
-
-  const server_url = import.meta.env.VITE_SERVER_URL;
-  const url = server_url + '/community/api/article/' + article_id;
-
-  useEffect(() => {
-    const getArticle = async () => {
-      try {
-        const response = await axios.get(url);
-        const res = response.data;
-        console.log(res);
-        if (res.status === 'success') {
-          setArticle(res.data.article);
-          setTags(res.data.tags);
-          setComments(res.data.comments);
-          setBoard(res.data.board);
-        }
-      } catch (error) {
-        setError(true);
-      }
-    };
-    getArticle();
-  }, []);
+  const article = props.article;
+  const tags = props.tags;
+  const comments = props.comments;
+  const board = props.board;
 
   return (
-    <>
-      {error_occur ? (
-        <>잘못된 페이지 입니다.</>
-      ) : (
-        <div id="community-main" className="col-md-9">
-          {need_login ? (
-            <div className="m-2">
-              로그인이 필요한 서비스입니다. <a href="/accounts/login/">바로가기</a>
-            </div>
-          ) : need_member ? (
-            <div className="m-2">
-              팀 멤버만 열람할 수 있습니다.
-              <a href={`/community/board/팀 모집/2`}>팀 모집 게시판 확인하기</a>
-            </div>
-          ) : (
-            <>
-              <div id="article-content">
-                {type === 'view' ? (
-                  <ContentView board={board} tags={tags} comments={comments} article={article} />
-                ) : (
-                  <>{/* <Content_Edit /> */}</>
-                )}
-              </div>
-              <div id="comment-content">{type !== 'register' && <Comment comments={comments} article={article} />}</div>
-            </>
-          )}
+    <div className="col-md-9 community-main">
+      {need_login && (
+        <div className="m-2">
+          로그인이 필요한 서비스입니다. <a href="/accounts/login/">바로가기</a>
         </div>
       )}
-    </>
+      {!need_login && need_member && (
+        <div className="m-2">
+          팀 멤버만 열람할 수 있습니다. <a href={`/community/board/팀 모집/2`}>팀 모집 게시판 확인하기</a>
+        </div>
+      )}
+      {!need_login && !need_member && (
+        <>
+          <div className="article-content">
+            {type === 'view' ? (
+              <ContentView board={board} tags={tags} comments={comments} article={article} />
+            ) : (
+              <>{/* <Content_Edit /> */}</>
+            )}
+          </div>
+          <div id="comment-content">{type !== 'register' && <Comment comments={comments} article={article} />}</div>
+        </>
+      )}
+    </div>
   );
 }
 export default Article_Presenter;
