adb487ab9c0a98f4c3b5ea8fbe3e822546cec448
wjieun <wjieun@g.skku.edu>
2023-08-27 00:23:27 +0900

    작성된 글 없는 게시판 예외 처리

--- frontend/src/components/Community/Board/BoardArticle/BoardArticle_Presenter.jsx
+++ frontend/src/components/Community/Board/BoardArticle/BoardArticle_Presenter.jsx
@@ -2,47 +2,53 @@ import '../Board.css';
 import { BsHandThumbsUp, BsFillChatLeftTextFill, BsBookmark, BsEyeFill } from 'react-icons/bs';
 
 export default function BoardArticle_Presenter(props) {
-  const { article, pubDate, onArticle, onWriter } = props;
+  const { username, article, pubDate, onArticle, onWriter } = props;
 
   return (
     <div className="board-article">
-      <h6>
-        {article.writer ? (
-          article.anonymous_writer ? (
-            <span>익명</span>
-          ) : (
-            <span className="board-article-writer" onClick={onWriter}>
-              {article.writer.user.username}
-            </span>
-          )
-        ) : (
-          <span>탈퇴한 이용자</span>
-        )}{' '}
-        · {pubDate}
-      </h6>
-      <h4 className="board-article-title" onClick={onArticle}>
-        {article.title}
-      </h4>
-      <div>
-        {article.tags.map((tag) => (
-          <h6 className="inline" key={tag.name}>
-            #{tag.name.replace(' ', '_')}&nbsp;
+      {article.title ? (
+        <>
+          <h6>
+            {article.writer ? (
+              article.anonymous_writer ? (
+                <span>익명</span>
+              ) : (
+                <span className={username && 'board-article-writer'} onClick={username && onWriter}>
+                  {article.writer.user.username}
+                </span>
+              )
+            ) : (
+              <span>탈퇴한 이용자</span>
+            )}{' '}
+            · {pubDate}
           </h6>
-        ))}
-        <div className="board-article-meta-list">
-          <>
-            <BsHandThumbsUp size={13} className="board-article-meta" /> {article.like_cnt}
-          </>
-          {/* <><BsFillChatLeftTextFill size={13} className='board-article-meta' /> {article.comment_cnt}</> */}
-          <>
-            <BsBookmark size={13} className="board-article-meta" /> {article.scrap_cnt}
-          </>
-          <>
-            <BsEyeFill size={13} className="board-article-meta" /> {article.view_cnt}
-          </>
-        </div>
-        <div className="float-clear"></div>
-      </div>
+          <h4 className="board-article-title" onClick={onArticle}>
+            {article.title}
+          </h4>
+          <div>
+            {article.tags.map((tag) => (
+              <h6 className="inline" key={tag.name}>
+                #{tag.name.replace(' ', '_')}&nbsp;
+              </h6>
+            ))}
+            <div className="board-article-meta-list">
+              <>
+                <BsHandThumbsUp size={13} className="board-article-meta" /> {article.like_cnt}
+              </>
+              {/* <><BsFillChatLeftTextFill size={13} className='board-article-meta' /> {article.comment_cnt}</> */}
+              <>
+                <BsBookmark size={13} className="board-article-meta" /> {article.scrap_cnt}
+              </>
+              <>
+                <BsEyeFill size={13} className="board-article-meta" /> {article.view_cnt}
+              </>
+            </div>
+            <div className="float-clear"></div>
+          </div>
+        </>
+      ) : (
+        <h5>작성된 글이 없습니다.</h5>
+      )}
     </div>
   );
 }
