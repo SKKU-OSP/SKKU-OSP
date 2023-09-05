51cd3e774c124db1d87931d3c49815bc686bf4a1
hsh <hsh200315@naver.com>
2023-09-05 11:40:13 +0900

    feat: 팀 지원서 수락,거절,삭제 기능 구현

--- frontend/src/components/NavBar/TeamApplication/TeamApplication.jsx
+++ frontend/src/components/NavBar/TeamApplication/TeamApplication.jsx
@@ -13,12 +13,10 @@ import { getAuthConfig } from '../../../utils/auth';
 import './TeamApplication.css';
 
 const serverUrl = import.meta.env.VITE_SERVER_URL;
+const applyUrl = serverUrl + '/team/api/team-apply-update/';
+const deleteUrl = serverUrl + '/team/api/team-apply-delete/';
 const TeamApplication = ({ handleClose, show }) => {
   const [activeIndex, setActiveIndex] = useState(0);
-
-  const tabClickHandler = (index) => {
-    setActiveIndex(index);
-  };
 
   const [received, setReceived] = useState([]);
   const [sent, setSent] = useState([]);
@@ -29,11 +27,13 @@ const TeamApplication = ({ handleClose, show }) => {
         const url = serverUrl + '/team/api/applications';
         const response = await axios.get(url, getAuthConfig());
         const res = response.data;
+        console.log(res);
         if (res.status === 'fail') {
           console.log(res.status, res.errors);
         } else {
           setReceived(res.data.received);
           setSent(res.data.sent);
+          console.log(res.data.sent);
         }
       } catch (error) {
         console.log(error);
@@ -43,6 +43,56 @@ const TeamApplication = ({ handleClose, show }) => {
       getApplications();
     }
   }, [show]);
+
+  //AXIOS POST
+  const sendTeamApplication = async (id, userId, status) => {
+    try {
+      const data = { target_teamapplymessage_id: id, is_okay: status, user_id: userId };
+      console.log(data);
+      const response = await axios.post(applyUrl, data, getAuthConfig());
+      const res = response.data;
+      if (res.status === 'fail') {
+        console.log(res.status, res.errors);
+      } else {
+        console.log(res.data);
+        alert('정말 하겠습니까');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const sendDeleteApplication = async (id) => {
+    try {
+      const data = { target_teamapplymessage_id: id };
+      console.log(data);
+      const response = await axios.post(deleteUrl, data, getAuthConfig());
+      const res = response.data;
+      if (res.status === 'fail') {
+        console.log(res.status, res.errors);
+      } else {
+        console.log(res.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const tabClickHandler = (index) => {
+    setActiveIndex(index);
+  };
+
+  const handleApproveClick = (applyId, userId) => {
+    sendTeamApplication(applyId, userId, 'true');
+  };
+
+  const handleRefuseClick = (applyId, userId) => {
+    sendTeamApplication(applyId, userId, 'false');
+  };
+
+  const handleDeleteClick = (applyId) => {
+    sendDeleteApplication(applyId);
+    setSent((prev) => prev.filter((app) => app.id !== applyId));
+  };
 
   const tabContArr = [
     {
@@ -64,14 +114,33 @@ const TeamApplication = ({ handleClose, show }) => {
                 className="d-flex justify-content-between align-items-center pt-2 pb-2 border border-start-0 border-end-0"
               >
                 <div className="d-flex">
-                  <div className="me-2"> {application.team.name} </div>
+                  <div className="me-2">
+                    {' '}
+                    {application.team.name} {application.team.id}{' '}
+                  </div>
                   <Link>{application.account.user.username}</Link>
                 </div>
                 <div>
-                  <Button variant="outline-danger" size="sm" className="text-dark me-2">
+                  <Button
+                    variant="outline-danger"
+                    size="sm"
+                    id={application.id}
+                    className="text-dark me-2"
+                    onClick={() => {
+                      handleRefuseClick(application.id, application.account.user.id);
+                    }}
+                  >
                     거절
                   </Button>
-                  <Button variant="outline-primary" size="sm" className="text-dark me-2">
+                  <Button
+                    variant="outline-primary"
+                    size="sm"
+                    id={application.id}
+                    className="text-dark me-2"
+                    onClick={() => {
+                      handleApproveClick(application.id, application.account.user.id);
+                    }}
+                  >
                     수락
                   </Button>
                 </div>
@@ -96,23 +165,26 @@ const TeamApplication = ({ handleClose, show }) => {
             sent.map((application) => (
               <div
                 key={`sent-${application.id}`}
-                className="d-flex justify-content-between align-items-center pt-2 pb-2 border border-start-0 border-end-0"
+                className="d-flex justify-content-between align-items-center pt-2 pb-2 border border-start-0 border-end-0 gap-2"
               >
-                <div className="d-flex">
-                  <div className="me-2"> {application.team.name} </div>
-                </div>
-                <div>
+                <div>{application.team.name}</div>
+                <div className="d-flex flex-row">
                   {application.status === 1 ? (
-                    <Badge bg="primary" className="text-center text-left fs-6 me-2">
+                    <Badge bg="primary" className="text-center text-left fs-6 me-2 lh-sm">
                       승인됨
                     </Badge>
                   ) : (
-                    <Badge bg="danger" className="text-center text-left fs-6 me-2">
+                    <Badge bg="danger" className="text-center text-left fs-6 me-2 lh-sm">
                       거절됨
                     </Badge>
                   )}
 
-                  <Button variant="outline-danger" size="sm" className="text-dark me-2">
+                  <Button
+                    variant="outline-danger"
+                    size="sm"
+                    className="text-dark me-2"
+                    onClick={() => handleDeleteClick(application.id)}
+                  >
                     삭제
                   </Button>
                 </div>
