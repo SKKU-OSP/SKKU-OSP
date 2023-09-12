d83c9832f8cf5d6801dbefab817040813f70531e
SeoJeongYeop <sjyswe99@gmail.com>
2023-09-12 18:02:30 +0900

    fix: 토큰 만료시 게시글 조회 불가능한 오류 수정 - 페이지네이션 리팩토링

--- frontend/src/components/Community/Board/CommunityNavItem/CommunityNavItem_Container.jsx
+++ frontend/src/components/Community/Board/CommunityNavItem/CommunityNavItem_Container.jsx
@@ -2,13 +2,12 @@ import CommunityNavItem_Presenter from './CommunityNavItem_Presenter';
 import { useNavigate } from 'react-router-dom';
 
 export default function CommunityNavItem_Container(props) {
-  const { navName, tabName, setNowPage } = props;
+  const { navName, tabName } = props;
   const navigate = useNavigate();
   const isActive = navName === tabName;
 
   const onNavigate = () => {
     navigate(`./../${navName}`);
-    setNowPage(1);
   };
 
   return <CommunityNavItem_Presenter isActive={isActive} navName={navName} onNavigate={onNavigate} />;
