3c260b5f8596d84f7f799dc57d47000bab35999e
wjieun <wjieun@g.skku.edu>
2023-09-21 13:05:17 +0900

    fix: editor 교체

--- frontend/src/components/Community/Article/ArticleRegister/ArticleRegister.jsx
+++ frontend/src/components/Community/Article/ArticleRegister/ArticleRegister.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Editor } from '@tinymce/tinymce-react';
 import axios from 'axios';
 import Select from 'react-select';
 import { BsXLg } from 'react-icons/bs';
@@ -8,6 +7,11 @@ import { Button } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { getAuthConfig } from '../../../../utils/auth';
+
+import ReactQuill, { Quill } from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import ImageResize from 'quill-image-resize';
+Quill.register('modules/ImageResize', ImageResize);
 
 import '../../Board/Board.css';
 
@@ -35,7 +39,6 @@ function ArticleRegister({ isWrite, type, consentWriteOpen }) {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const getCurrentDateTime = () => new Date();
-  const editorRef = useRef(null);
   const [isTeamRegistration, setIsTeamRegistration] = useState(false);
 
   useEffect(() => {
@@ -115,20 +118,17 @@ function ArticleRegister({ isWrite, type, consentWriteOpen }) {
       window.alert('본문을 입력해 주세요');
       return;
     } else if (window.confirm('글을 등록하시겠습니까?')) {
-      // 글 등록
-      if (editorRef.current) {
-        console.log('title', title);
-        console.log('bodyText', bodyText);
-        console.log('tags', selectTags);
-        console.log('ano', anonymousWriter);
-        console.log('isn', isAuthNotice);
-        console.log('ps', startDate);
-        console.log('pe', endDate);
-        console.log('team', team);
-        console.log('articleFile', articleFile);
+      console.log('title', title);
+      console.log('bodyText', bodyText);
+      console.log('tags', selectTags);
+      console.log('ano', anonymousWriter);
+      console.log('isn', isAuthNotice);
+      console.log('ps', startDate);
+      console.log('pe', endDate);
+      console.log('team', team);
+      console.log('articleFile', articleFile);
 
-        postArticle();
-      }
+      postArticle();
     }
   };
 
@@ -191,11 +191,7 @@ function ArticleRegister({ isWrite, type, consentWriteOpen }) {
 
   // Body
   const handleBodyChange = (e) => {
-    setBodyText(e.target.getContent());
-  };
-
-  const handleEditorInit = (editor) => {
-    editorRef.current = editor;
+    setBodyText(e);
   };
 
   // File
@@ -340,6 +336,66 @@ function ArticleRegister({ isWrite, type, consentWriteOpen }) {
     </div>
   );
 
+  const modules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ font: [] }],
+        [{ align: [] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
+        [
+          {
+            color: [
+              '#000000',
+              '#e60000',
+              '#ff9900',
+              '#ffff00',
+              '#008a00',
+              '#0066cc',
+              '#9933ff',
+              '#ffffff',
+              '#facccc',
+              '#ffebcc',
+              '#ffffcc',
+              '#cce8cc',
+              '#cce0f5',
+              '#ebd6ff',
+              '#bbbbbb',
+              '#f06666',
+              '#ffc266',
+              '#ffff66',
+              '#66b966',
+              '#66a3e0',
+              '#c285ff',
+              '#888888',
+              '#a10000',
+              '#b26b00',
+              '#b2b200',
+              '#006100',
+              '#0047b2',
+              '#6b24b2',
+              '#444444',
+              '#5c0000',
+              '#663d00',
+              '#666600',
+              '#003700',
+              '#002966',
+              '#3d1466',
+              'custom-color'
+            ]
+          },
+          { background: [] }
+        ],
+        ['image', 'video'],
+        ['clean']
+      ]
+    },
+    ImageResize: {
+      parchment: Quill.import('parchment')
+    }
+  };
+
   return (
     <>
       {renderConsentMessage}
@@ -428,29 +484,11 @@ function ArticleRegister({ isWrite, type, consentWriteOpen }) {
               )}
             </>
           )}
-          <Editor
-            initialValue={bodyText}
-            onInit={handleEditorInit}
+          <ReactQuill
+            value={bodyText}
             onChange={handleBodyChange}
-            init={{
-              height: 350,
-              branding: false,
-              statusbar: false,
-              paste_data_images: true,
-              plugins: [
-                'advlist autolink lists link image charmap print preview anchor',
-                'searchreplace visualblocks code fullscreen',
-                'insertdatetime media table paste code help wordcount',
-                'undo',
-                'redo'
-              ],
-              toolbar:
-                'undo redo | fontfamily ' +
-                'bold italic underline strikethrough forecolor backcolor align | ' +
-                'removeformat help| image',
-              forced_root_block: 'div'
-            }}
-            style={{ zIndex: 1 }}
+            modules={modules}
+            style={{ minHeight: '350px', zIndex: '1' }}
           />
 
           <Select
