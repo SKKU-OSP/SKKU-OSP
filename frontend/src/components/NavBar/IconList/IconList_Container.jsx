9a6abd30a3fe86800c0998fbf6b529a6ce9acb56
hsh <hsh200315@naver.com>
2023-10-04 10:01:28 +0900

    feat: admin nav 구현

--- frontend/src/components/NavBar/IconList/IconList_Container.jsx
+++ frontend/src/components/NavBar/IconList/IconList_Container.jsx
@@ -1,9 +1,9 @@
 import IconList_Presenter from './IconList_Presenter';
 
-function IconList_Container() {
+function IconList_Container(props) {
   const iconSize = '24';
 
-  return <IconList_Presenter iconSize={iconSize} />;
+  return <IconList_Presenter iconSize={iconSize} username={props.username} />;
 }
 
 export default IconList_Container;
--- frontend/src/components/NavBar/IconList/IconList_Presenter.jsx
+++ frontend/src/components/NavBar/IconList/IconList_Presenter.jsx
@@ -2,17 +2,17 @@ import ChatMessage_Container from '../Chat/ChatMessage_Container';
 import Notification_Container from '../Notification/Notification_Container';
 import TeamApplication_Container from '../TeamApplication/TeamApplication_Container';
 import Profile_Container from '../Profile/Profile_Container';
+import AdminDropDown from '../adminNav/adminDropDown';
 import { useScrollTrigger } from '@mui/material';
 import { useState } from 'react';
 
-function IconList_Presenter(props) {
-  const { iconSize } = props;
-
+function IconList_Presenter({ iconSize, username }) {
   const [showTeamApp, setShowTeamApp] = useState(false);
   const [showNoti, setShowNoti] = useState(false);
-
+  console.log(typeof username);
   return (
     <div className="nav-bar-icon-list">
+      {username === 'admin' ? <AdminDropDown iconSize={iconSize} /> : <></>}
       <TeamApplication_Container iconSize={iconSize} showTeamApp={showTeamApp} setShowTeamApp={setShowTeamApp} />
       <ChatMessage_Container iconSize={iconSize} />
       <Notification_Container
