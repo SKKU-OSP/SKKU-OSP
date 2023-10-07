eb276b78c43f307aba0b4cf5d9abe17cf5c5ebbf
SeoJeongYeop <sjyswe99@gmail.com>
2023-10-07 20:45:19 +0900

    feat: 개발자 유형에 따른 상생/상극 파트너 카드 추가

--- frontend/src/components/User/DashBoard/DevType.jsx
+++ frontend/src/components/User/DashBoard/DevType.jsx
@@ -1,7 +1,48 @@
+import DevTypeCard from '../TypeTest/DevTypeCard';
+import ImageDescBox from './ImageDescBox';
+
+const serverUrl = import.meta.env.VITE_SERVER_URL;
 function DevType(props) {
   const data = props.data;
+  const devType = `${serverUrl}/static/images/${data.code}.png`;
+  const factors = [data.typeA, data.typeB, data.typeC, data.typeD];
+  const pos = data.pos;
+  const neg = data.neg;
+
   console.log('DevType', data);
-  return <div>DevType</div>;
+  return (
+    <div>
+      <DevTypeCard
+        devType={devType}
+        descEng={data.desc}
+        descKr={data.descKR}
+        typeEng={data.nickname}
+        typeKr={data.nicknameKR}
+        factors={factors}
+      />
+      <div className="d-flex justify-content-around">
+        <div>
+          <div className="fs-5">상생 파트너</div>
+          <ImageDescBox
+            src={`${serverUrl}/static/images/${pos.code}.png`}
+            title={pos.nicknameKR}
+            desc={pos.descKR}
+            attrs={pos.desc.split(' ')}
+          />
+        </div>
+        <div>
+          <div className="fs-5">상극 파트너</div>
+
+          <ImageDescBox
+            src={`${serverUrl}/static/images/${neg.code}.png`}
+            title={neg.nicknameKR}
+            desc={neg.descKR}
+            attrs={neg.desc.split(' ')}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DevType;
