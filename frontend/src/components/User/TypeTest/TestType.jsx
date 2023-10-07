038ed0a8b5dd0d2d37702295f028df28e45e865e
SeoJeongYeop <sjyswe99@gmail.com>
2023-10-07 15:17:41 +0900

    feat: 개발자 유형 ID 카드 컴포넌트 분리 - DevTypeCard 컴포넌트 추가 - 서버 주소를 .env에 의존하도록 변경 - 버튼 스타일 변경

--- frontend/src/components/User/TypeTest/TestType.jsx
+++ frontend/src/components/User/TypeTest/TestType.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Button, ProgressBar } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { ProgressBar } from 'react-bootstrap';
 
-import ResultProgress from './ResultProgress';
+import DevTypeCard from './DevTypeCard';
 import { qnaList, descList, resultList } from './TypeData';
 import './TestType.css';
 
+const serverUrl = import.meta.env.VITE_SERVER_URL;
 const TestType = () => {
   const [idx, setIdx] = useState(0);
   const [progressLen, setProgressLen] = useState(0);
@@ -90,21 +91,25 @@ const TestType = () => {
     for (let k = 0; k < 16; k++) {
       if (tmp == resultList[k].mbti) setResultIdx(k);
     }
-    setDevType(`http://127.0.0.1:8000/static/images/${tmp}.png`);
+    setDevType(`${serverUrl}/static/images/${tmp}.png`);
   };
 
   useEffect(() => {
     console.log(devType);
   }, [devType]);
 
+  const bgImgStyle = {
+    backgroundImage: `url('${serverUrl}/static/images/back.png')`
+  };
+
   return (
     <div className="container">
       <section id="main">
-        <div className="board mx-0 mt-5">
+        <div className="board mx-0 mt-5" style={bgImgStyle}>
           <div className="my-3">
             <h3>개발자 유형 검사</h3>
           </div>
-          <div className={'d-flex justify-content-center'}>
+          <div className="d-flex justify-content-center">
             <span className="endpoint left"></span>
             <span className="line-connect"></span>
             <span className="endpoint right"></span>
@@ -117,21 +122,21 @@ const TestType = () => {
             결과는 추천 시스템과 무관하니 참고용으로 즐겨주시길 바랍니다
           </div>
           <div className="my-2">
-            <Button id="gbti-test-start" onClick={begin}>
+            <button className="btn btn-test-start" onClick={begin}>
               시작하기
-            </Button>
+            </button>
           </div>
         </div>
       </section>
       <section id="qna" className="m-0">
         <div className="qbox my-5 py-3">{qnaList[idx].q}</div>
         <div className="abox">
-          <Button className="answerbtn mb-2" onClick={() => clickAnswer(0)}>
+          <button className="answerbtn mb-2" onClick={() => clickAnswer(0)}>
             {qnaList[idx].answer[0].a}
-          </Button>
-          <Button className="answerbtn mt-1" onClick={() => clickAnswer(1)}>
+          </button>
+          <button className="answerbtn mt-1" onClick={() => clickAnswer(1)}>
             {qnaList[idx].answer[1].a}
-          </Button>
+          </button>
         </div>
         <ProgressBar className="progress mt-5" now={progressLen}></ProgressBar>
       </section>
@@ -142,58 +147,14 @@ const TestType = () => {
           <span className="line-connect"></span>
           <span className="endpoint right"></span>
         </div>
-        <div id="gbti-id-card" style={{ position: 'relative' }}>
-          <div id="gbti-id-card-content" className="modal-gbti-card">
-            <p className="weak-text ms-3 my-2 text-start">OSS ID CARD</p>
-            <div className="d-flex justify-content-center">
-              <div className="gbti-content d-flex justify-content-center" style={{ padding: '1% 4%', width: '50%' }}>
-                <img src={devType} className="gbti-img" />
-              </div>
-              <div
-                className="gbti-content d-flex flex-column justify-content-between"
-                style={{ padding: '1% 4%', width: '50%' }}
-              >
-                <p
-                  id="descEng"
-                  className="text-center text-label"
-                  style={{ fontSize: '12px', marginBottom: '2px', fontWeight: '600' }}
-                >
-                  {descEng}
-                </p>
-                <p id="descKr" className="text-center text-label mb-1" style={{ fontSize: '14px', fontWeight: '600' }}>
-                  {descKr}
-                </p>
-                <h4 id="typeEng" className="text-center">
-                  {resultList[resultIdx].typeEng}
-                </h4>
-                <h4 id="typeKr" className="text-center">
-                  {resultList[resultIdx].typeKr}
-                </h4>
-                <div className="d-flex justify-content-between text-label">
-                  <span>Fluid</span>
-                  <span>Steady</span>
-                </div>
-                <ResultProgress factor={factor[0]} />
-                <div className="d-flex justify-content-between text-label">
-                  <span>CoolHead</span>
-                  <span>WarmHeart</span>
-                </div>
-                <ResultProgress factor={factor[1]} />
-                <div className="d-flex justify-content-between text-label">
-                  <span>Creative</span>
-                  <span>Efficient</span>
-                </div>
-                <ResultProgress factor={factor[2]} />
-                <div className="d-flex justify-content-between text-label">
-                  <span>MultiPlay</span>
-                  <span>SinglePlay</span>
-                </div>
-                <ResultProgress factor={factor[3]} />
-              </div>
-            </div>
-            <p className="weak-text me-3 my-2 text-end">2023 &#9426; SKKU-OSP</p>
-          </div>
-        </div>
+        <DevTypeCard
+          devType={devType}
+          descEng={descEng}
+          descKr={descKr}
+          typeEng={resultList[resultIdx].typeEng}
+          typeKr={resultList[resultIdx].typeKr}
+          factors={factor}
+        />
       </section>
     </div>
   );
