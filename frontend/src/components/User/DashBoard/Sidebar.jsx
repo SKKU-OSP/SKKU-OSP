5269aa3d145189da841bc67934b2e8d4bb6a92ab
SeoJeongYeop <sjyswe99@gmail.com>
2023-10-09 14:36:45 +0900

    feat: 대시보드에 사이드바 추가

---
+++ frontend/src/components/User/DashBoard/Sidebar.jsx
@@ -0,0 +1,50 @@
+import { NavLink, useParams } from 'react-router-dom';
+import { BsList, BsFillPersonFill, BsFillBarChartFill } from 'react-icons/bs';
+import { FaShapes } from 'react-icons/fa6';
+
+function Sidebar(props) {
+  const onToggle = props.onToggle;
+  const username = useParams().username;
+  const activeStyle = { color: 'black' };
+  return (
+    <div id="sidebar-wrapper">
+      <ul className="sidebar-nav" style={{ marginLeft: 0 }}>
+        <li className="sidebar-toggle pt-2">
+          <BsList size={24} id="menu-toggle" style={{ float: 'right' }} onClick={onToggle} />
+        </li>
+        <li>
+          <NavLink
+            to={`/user/${username}/profile`}
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            className="text-nowrap"
+          >
+            <BsFillPersonFill size={24} />
+            <span className="sidebar-menu">프로필</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={`/user/${username}/dashboard`}
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            className="text-nowrap"
+          >
+            <BsFillBarChartFill size={24} />
+            <span className="sidebar-menu">대시보드</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={`/user/${username}/dev-type`}
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            className="text-nowrap"
+          >
+            <FaShapes size={24} />
+            <span className="sidebar-menu">개발자 유형</span>
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
