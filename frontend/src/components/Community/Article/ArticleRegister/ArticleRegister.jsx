56f53c6ede5cdd06d1cfebe7a1011369a2d7c05c
wjieun <wjieun@g.skku.edu>
2023-10-10 17:33:51 +0900

    fix: article register/edit

--- frontend/src/components/Community/Article/ArticleRegister/ArticleRegister.jsx
+++ frontend/src/components/Community/Article/ArticleRegister/ArticleRegister.jsx
@@ -7,11 +7,8 @@ import { Button } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { getAuthConfig } from '../../../../utils/auth';
-
-import ReactQuill, { Quill } from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import ImageResize from 'quill-image-resize';
-Quill.register('modules/ImageResize', ImageResize);
+import { EditorModules } from '../editor';
+import ReactQuill from 'react-quill';
 
 import '../../Board/Board.css';
 
@@ -173,13 +170,7 @@ function ArticleRegister({ isWrite, type, consentWriteOpen }) {
       console.log(res);
       if (res['status'] === 'success') {
         window.alert('등록이 완료되었습니다!');
-        if (boardName === '팀 모집') {
-          navigate(`/community/recruit/${boardName}/`);
-        } else if (boardName === '자유' || boardName === '정보' || boardName === '질문' || boardName === '홍보') {
-          navigate(`/community/board/${boardName}/`);
-        } else {
-          navigate(`/community/team/${boardName}/`);
-        }
+        navigate(`/community/article/${res.data.article_id}`);
       } else {
         window.alert(res['message']);
         return;
@@ -342,66 +333,6 @@ function ArticleRegister({ isWrite, type, consentWriteOpen }) {
     </div>
   );
 
-  const modules = {
-    toolbar: {
-      container: [
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
-        [{ font: [] }],
-        [{ align: [] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
-        [
-          {
-            color: [
-              '#000000',
-              '#e60000',
-              '#ff9900',
-              '#ffff00',
-              '#008a00',
-              '#0066cc',
-              '#9933ff',
-              '#ffffff',
-              '#facccc',
-              '#ffebcc',
-              '#ffffcc',
-              '#cce8cc',
-              '#cce0f5',
-              '#ebd6ff',
-              '#bbbbbb',
-              '#f06666',
-              '#ffc266',
-              '#ffff66',
-              '#66b966',
-              '#66a3e0',
-              '#c285ff',
-              '#888888',
-              '#a10000',
-              '#b26b00',
-              '#b2b200',
-              '#006100',
-              '#0047b2',
-              '#6b24b2',
-              '#444444',
-              '#5c0000',
-              '#663d00',
-              '#666600',
-              '#003700',
-              '#002966',
-              '#3d1466',
-              'custom-color'
-            ]
-          },
-          { background: [] }
-        ],
-        ['image', 'video'],
-        ['clean']
-      ]
-    },
-    ImageResize: {
-      parchment: Quill.import('parchment')
-    }
-  };
-
   return (
     <>
       {renderConsentMessage}
@@ -493,7 +424,7 @@ function ArticleRegister({ isWrite, type, consentWriteOpen }) {
           <ReactQuill
             value={bodyText}
             onChange={handleBodyChange}
-            modules={modules}
+            modules={EditorModules}
             style={{ minHeight: '350px', zIndex: '1' }}
           />
 
