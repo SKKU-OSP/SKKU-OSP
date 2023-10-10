0ffa083a48f2786c05e2f629f01d5016c6191d0f
wjieun <wjieun@g.skku.edu>
2023-10-10 17:33:51 +0900

    fix: article register/edit

--- frontend/src/components/Community/Article/ArticleEdit/ArticleEdit.jsx
+++ frontend/src/components/Community/Article/ArticleEdit/ArticleEdit.jsx
@@ -7,11 +7,8 @@ import LoaderIcon from 'react-loader-icon';
 import DatePicker from 'react-datepicker';
 import AuthContext from '../../../../utils/auth-context';
 import { getAuthConfig } from '../../../../utils/auth';
-
-import ReactQuill, { Quill } from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import ImageResize from 'quill-image-resize';
-Quill.register('modules/ImageResize', ImageResize);
+import { EditorModules } from '../editor';
+import ReactQuill from 'react-quill';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import './ArticleEdit.css';
@@ -330,66 +327,6 @@ function ArticleEdit({ isWrite, type, consentWriteOpen }) {
     </div>
   );
 
-  const modules = {
-    toolbar: {
-      container: [
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
-        [{ font: [] }],
-        [{ align: [] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
-        [
-          {
-            color: [
-              '#000000',
-              '#e60000',
-              '#ff9900',
-              '#ffff00',
-              '#008a00',
-              '#0066cc',
-              '#9933ff',
-              '#ffffff',
-              '#facccc',
-              '#ffebcc',
-              '#ffffcc',
-              '#cce8cc',
-              '#cce0f5',
-              '#ebd6ff',
-              '#bbbbbb',
-              '#f06666',
-              '#ffc266',
-              '#ffff66',
-              '#66b966',
-              '#66a3e0',
-              '#c285ff',
-              '#888888',
-              '#a10000',
-              '#b26b00',
-              '#b2b200',
-              '#006100',
-              '#0047b2',
-              '#6b24b2',
-              '#444444',
-              '#5c0000',
-              '#663d00',
-              '#666600',
-              '#003700',
-              '#002966',
-              '#3d1466',
-              'custom-color'
-            ]
-          },
-          { background: [] }
-        ],
-        ['image', 'video'],
-        ['clean']
-      ]
-    },
-    ImageResize: {
-      parchment: Quill.import('parchment')
-    }
-  };
-
   return (
     <>
       {myArticle ? (
@@ -472,7 +409,7 @@ function ArticleEdit({ isWrite, type, consentWriteOpen }) {
               <ReactQuill
                 value={bodyText}
                 onChange={handleBodyChange}
-                modules={modules}
+                modules={EditorModules}
                 style={{ minHeight: '350px', zIndex: '1' }}
               />
               <Select
