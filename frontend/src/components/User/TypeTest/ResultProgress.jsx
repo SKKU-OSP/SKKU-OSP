b5b75ba3d4a11ea2a48ff13c301dbefe1e0cc0dc
hsh <hsh200315@naver.com>
2023-10-24 14:21:23 +0900

    feat: 개발자 유형 검사 progress bar 색상 수정

--- frontend/src/components/User/TypeTest/ResultProgress.jsx
+++ frontend/src/components/User/TypeTest/ResultProgress.jsx
@@ -7,10 +7,10 @@ const ResultProgress = (factor) => {
   const rightLength = String((100 + q) / 2 + ((100 + q) % 2) - 3) + '%';
   console.log(leftLength, rightLength, factor.factor);
   return (
-    <div className="d-flex">
-      <div style={{ height: '10px', width: leftLength, backgroundColor: 'rgb(0, 39, 67)' }}></div>
-      <div style={{ height: '10px', width: '6%', backgroundColor: 'yellow' }}></div>
-      <div style={{ height: '10px', width: rightLength, backgroundColor: 'rgb(0, 39, 67)' }}></div>
+    <div className="d-flex" style={{ backgroundColor: '#E2E2E2' }}>
+      <div style={{ height: '10px', width: leftLength, backgroundColor: '#E2E2E2' }}></div>
+      <div style={{ height: '10px', width: '6%', backgroundColor: 'rgb(0, 39, 67)', borderRadius: '4px' }}></div>
+      <div style={{ height: '10px', width: rightLength, backgroundColor: '#E2E2E2' }}></div>
     </div>
     // <ProgressBar>
     //   <ProgressBar className="progress" now={leftLength} key={1} />
