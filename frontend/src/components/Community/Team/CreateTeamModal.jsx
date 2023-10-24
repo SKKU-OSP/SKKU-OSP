e3591ea915797a38603525d7e85aa7b19f765758
hsh <hsh200315@naver.com>
2023-10-24 15:16:52 +0900

    feat: 팀 만들기 tag post 추가

--- frontend/src/components/Community/Team/CreateTeamModal.jsx
+++ frontend/src/components/Community/Team/CreateTeamModal.jsx
@@ -1,17 +1,91 @@
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Select from 'react-select';
 
+import { getAuthConfig } from '../../../utils/auth';
+
 const CreateTeamModal = ({ show, onClose }) => {
-  const options = [
-    { value: 'Fastify', label: 'Fastify' },
-    { value: 'Fastlane', label: 'Fastlane' },
-    { value: 'Flask', label: 'Flask' },
-    { value: 'Flink', label: 'Flink' },
-    { value: 'Fluentd', label: 'Fluentd' },
-    { value: 'Flutter', label: 'Flutter' }
-  ];
+  const domainUrl = import.meta.env.VITE_SERVER_URL;
+  const postCreateUrl = domainUrl + '/team/api/team-create';
+
+  const [options, setOptions] = useState([]);
+  const [imgFile, setImgFile] = useState('');
+  const [selectTags, setSelectTags] = useState([]);
+
+  const teamnameInputRef = useRef();
+  const teamdescriptionInputRef = useRef();
+  const teamImgRef = useRef();
+
+  useEffect(() => {
+    const getTag = async () => {
+      const urlTag = domainUrl + '/tag/api/list/';
+      const responseTag = await axios.get(urlTag);
+
+      const resTag = responseTag.data;
+      if (resTag.status === 'success') {
+        setOptions(
+          resTag.data.tags.map((t) => {
+            return {
+              value: t.name,
+              label: t.name,
+              color: t.color
+            };
+          })
+        );
+      } else {
+        console.log(resTag.message);
+      }
+    };
+    getTag();
+  }, []);
+
+  const postCreateTeam = async () => {
+    try {
+      const postData = {
+        team_name: teamnameInputRef.current.value,
+        team_description: teamdescriptionInputRef.current.value,
+        team_image: teamImgRef.current.files[0],
+        team_tag: selectTags
+      };
+      const formData = new FormData();
+      if ((teamnameInputRef.current.value === '') | (teamdescriptionInputRef.current.value === '')) return;
+      Object.entries(postData).forEach(([key, value]) => {
+        formData.append(key, value);
+      });
+      const response = await axios.post(postCreateUrl, formData, getAuthConfig());
+      const res = response.data;
+      if (res.status === 'fail') {
+        console.log(res.status, res.errors);
+      } else {
+        console.log(res.data);
+      }
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  const handleSaveClick = () => {
+    postCreateTeam();
+    onClose();
+  };
+
+  const saveImgFile = () => {
+    const file = teamImgRef.current.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImgFile(reader.result);
+    };
+  };
+
+  const handleSelectTag = (selectTags) => {
+    setSelectTags(selectTags.map((tag) => tag.value));
+  };
+
   return (
     <Modal show={show} onHide={onClose}>
       <Modal.Header closeButton>
@@ -23,31 +97,38 @@ const CreateTeamModal = ({ show, onClose }) => {
       <Modal.Body>
         <Form.Group className="mb-4" controlId="TeamName">
           <Form.Label>팀 이름</Form.Label>
-          <Form.Control type="text" style={{ display: 'block' }} />
+          <Form.Control ref={teamnameInputRef} type="text" style={{ display: 'block' }} />
         </Form.Group>
         <Form.Group className="mb-4" controlId="TeamExplain">
           <Form.Label>팀 설명</Form.Label>
-          <Form.Control as="textarea" rows={3} style={{ display: 'block' }} />
+          <Form.Control ref={teamdescriptionInputRef} as="textarea" rows={3} style={{ display: 'block' }} />
         </Form.Group>
 
         <Form.Group className="mb-4" controlId="TeamImg">
           <Form.Label>팀 대표 이미지</Form.Label>
           <div style={{ fontSize: 'small', color: 'gray', marginBottom: '10px' }}>png,jpg,jpeg 확장자만 지원합니다</div>
-          <Form.Control type="file" accept=".png, .jpg, .jpeg" multiple style={{ display: 'block' }} />
+          <Form.Control
+            ref={teamImgRef}
+            onChange={saveImgFile}
+            type="file"
+            accept=".png, .jpg, .jpeg"
+            multiple
+            style={{ display: 'block' }}
+          />
         </Form.Group>
 
         <div className="mb-4">
           <span style={{ display: 'block' }}>이미지 미리보기</span>
-          <img width="80px" height="80px" src="../images/chunsik.webp" alt="img" />
+          <img width="80px" height="80px" src={imgFile ? imgFile : null} alt="img" />
         </div>
         <div className="mb-4">
           <span style={{ display: 'block' }}>팀 분야</span>
-          <Select isMulti options={options} />
+          <Select isMulti options={options} onChange={handleSelectTag} />
         </div>
       </Modal.Body>
 
       <Modal.Footer>
-        <Button variant="primary" onClick={onClose}>
+        <Button variant="primary" onClick={handleSaveClick}>
           저장
         </Button>
       </Modal.Footer>
