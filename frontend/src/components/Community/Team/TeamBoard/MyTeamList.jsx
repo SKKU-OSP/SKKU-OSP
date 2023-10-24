147af6dfb3cc961d0b1f04dc1b6065f497350367
m1nse0kim <ms0125@g.skku.edu>
2023-10-24 17:12:13 +0900

    feat: 내 팀 목록 페이지네이션 적용 및 비로그인 상태 접근 시 로그인 페이지로 이동

--- frontend/src/components/Community/Team/TeamBoard/MyTeamList.jsx
+++ frontend/src/components/Community/Team/TeamBoard/MyTeamList.jsx
@@ -5,6 +5,7 @@ import CreateTeamModal from '../CreateTeamModal';
 import { getAuthConfig } from '../../../../utils/auth';
 import { BsAwardFill, BsPeopleFill } from 'react-icons/bs';
 import LoaderIcon from 'react-loader-icon';
+import Pagination from 'react-js-pagination';
 import AuthContext from '../../../../utils/auth-context';
 import ProfileDropdown_Container from '../../ProfileDropdown';
 
@@ -13,39 +14,45 @@ const server_url = import.meta.env.VITE_SERVER_URL;
 function MyTeamList() {
   const navigate = useNavigate();
   const [myTeams, setMyTeams] = useState([]);
+  const [maxPage, setMaxPage] = useState(0);
+  const [nowPage, setNowPage] = useState(1);
   const [error, setError] = useState(false);
   const [modalShow, setModalShow] = useState(false);
   const { username } = useContext(AuthContext);
 
-  const handleLogin = async () => {
-    if (!username) {
-      const shouldNavigate = await new Promise((resolve) => {
-        const confirmation = window.confirm('로그인해야 이용할 수 있는 기능입니다. 로그인 화면으로 이동하시겠습니까?');
-        resolve(confirmation);
-      });
-
-      if (shouldNavigate) {
-        navigate('/accounts/login');
+  const getMyTeamList = async (page) => {
+    try {
+      const responseTeamList = await axios.get(
+        server_url + `/team/api/teams-of-user-list/?page_number=${page}`,
+        getAuthConfig()
+      );
+      const resTeamList = responseTeamList.data;
+      if (resTeamList.status === 'success') {
+        setMyTeams(resTeamList.data.teams_of_user);
+        setMaxPage(resTeamList.data.max_page_number);
+        setNowPage(page);
       }
+    } catch (error) {
+      setError(true);
+      console.log('error', error);
     }
   };
 
   useEffect(() => {
-    handleLogin();
-    const getMyTeamList = async () => {
-      try {
-        const responseTeamList = await axios.get(server_url + `/team/api/teams-of-user-list/`, getAuthConfig());
-        const resTeamList = responseTeamList.data;
-        if (resTeamList.status === 'success') {
-          setMyTeams(resTeamList.data.teams_of_user);
-        }
-      } catch (error) {
-        setError(true);
-        console.log('error', error);
+    console.log('username', username);
+    if (username !== null) {
+      getMyTeamList(1);
+    } else {
+      if (window.confirm('로그인해야 이용할 수 있는 기능입니다. 로그인 화면으로 이동하시겠습니까?')) {
+        navigate('/accounts/login');
       }
-    };
-    getMyTeamList();
-  }, []);
+      return;
+    }
+  }, [username]);
+
+  const onPageChange = (page) => {
+    getTeamList(page);
+  };
 
   const handleShow = () => setModalShow(true);
   const handleClose = () => setModalShow(false);
@@ -67,28 +74,37 @@ function MyTeamList() {
       </div>
 
       {myTeams && myTeams.length > 0 ? (
-        myTeams.map((team) => (
-          <div className="board-article" key={team.id}>
-            <div>
-              <Link className="board-article-title" to={`/community/team/${team.name}`}>
-                <h4 className="board-article-title">{team.name}</h4>
-              </Link>
-            </div>
-            <div>
-              <div className="inline-block">{team.description}</div>
-              <div className="text-end">
-                {team.leader_username && (
-                  <>
-                    <BsAwardFill />
-                    <ProfileDropdown_Container userName={team.leader_username} userId={team.leader_id} />
-                  </>
-                )}
-                <BsPeopleFill />
-                {` ${team.member_cnt}명`}
+        <>
+          {myTeams.map((team) => (
+            <div className="board-article" key={team.id}>
+              <div>
+                <Link className="board-article-title" to={`/community/team/${team.name}`}>
+                  <h4 className="board-article-title">{team.name}</h4>
+                </Link>
+              </div>
+              <div>
+                <div className="inline-block">{team.description}</div>
+                <div className="text-end">
+                  {team.leader_username && (
+                    <>
+                      <BsAwardFill />
+                      <ProfileDropdown_Container userName={team.leader_username} userId={team.leader_id} />
+                    </>
+                  )}
+                  <BsPeopleFill />
+                  {` ${team.member_cnt}명`}
+                </div>
               </div>
             </div>
-          </div>
-        ))
+          ))}
+          <Pagination
+            activePage={nowPage}
+            itemsCountPerPage={10}
+            totalItemsCount={maxPage * 10}
+            pageRangeDisplayed={5}
+            onChange={onPageChange}
+          />
+        </>
       ) : (
         <LoaderIcon style={{ marginTop: '20px' }} />
       )}
