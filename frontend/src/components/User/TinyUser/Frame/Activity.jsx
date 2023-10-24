be9624d0afecdfe76ae2356c67fa01a78cd71c46
605s <ploki7335@naver.com>
2023-10-24 17:30:32 +0900

    feat: 유저 프로필 페이지 css 수정

--- frontend/src/components/User/TinyUser/Frame/Activity.jsx
+++ frontend/src/components/User/TinyUser/Frame/Activity.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { getAuthConfig } from '../../../../utils/auth';
 import { BsStar, BsCheck2, BsArrow90DegLeft } from 'react-icons/bs';
@@ -12,6 +12,7 @@ function Activity(props) {
   const [profileActivity, setProfileActivity] = useState();
   const [editProfileActivity, setEditProfileActivity] = useState();
   const [editing, setEditing] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getProfileActivity = async () => {
@@ -49,6 +50,9 @@ function Activity(props) {
       [name]: value
     });
   };
+  const handleUrlClick = (url) => {
+    navigate(url);
+  };
 
   return (
     <>
@@ -59,8 +63,10 @@ function Activity(props) {
               <span className="intro">포트폴리오</span>
               {isEdit &&
                 (editing ? (
-                  <button className="btn" onClick={handleSaveClick}>
-                    <span className="btn-text">저장</span>
+                  <button className="btn" onClick={handleSaveClick} style={{ backgroundColor: 'black' }}>
+                    <span className="btn-text" style={{ color: 'white' }}>
+                      저장
+                    </span>
                   </button>
                 ) : (
                   <button className="btn" onClick={handleEditClick}>
@@ -72,21 +78,27 @@ function Activity(props) {
               {editing ? (
                 <textarea
                   name="portfolio"
-                  rows="3"
-                  style={{ width: '100%' }}
+                  rows="4"
+                  style={{ width: '100%', whiteSpace: 'pre-wrap' }}
                   value={editProfileActivity.portfolio}
                   onChange={handleInputChange}
                 />
               ) : (
                 <div className="text">
-                  {profileActivity.portfolio.split('\n').map((line) => {
-                    return (
-                      <span>
-                        {line}
-                        <br />
-                      </span>
-                    );
-                  })}
+                  {profileActivity.portfolio.length > 0 ? (
+                    <>
+                      {profileActivity.portfolio.split('\n').map((line, idx) => {
+                        return (
+                          <span key={`portfolio-${idx}`}>
+                            {line}
+                            <br />
+                          </span>
+                        );
+                      })}
+                    </>
+                  ) : (
+                    <>포트폴리오를 작성하지 않았습니다.</>
+                  )}
                 </div>
               )}
             </div>
@@ -94,35 +106,47 @@ function Activity(props) {
           <div className="d-flex flex-column justify-content-start profile-contribution">
             <div className="d-flex flex-row justify-content-between contribution-intro">
               <span className="intro">최근 기여활동</span>
-              <a href={`https://github.com/${githubId}`} className="href">
+              <a
+                onClick={() => {
+                  handleUrlClick(`/repository/${username}`);
+                }}
+                className="href"
+                style={{ cursor: 'pointer' }}
+              >
                 자세히 알아보기 &gt;
               </a>
             </div>
-            {profileActivity.recent_repos.map((repo, idx) => {
-              return (
-                <div className="contribution-repo" key={idx}>
-                  <div className="d-flex flex-column justify-content-start repo">
-                    <div className="d-flex flex-row justify-content-between title">
-                      <span className="title-text">{repo.repo_name}</span>
-                      <span className="commit-date">
-                        최근 커밋일자
-                        <br />
-                        {repo.committer_date.substring(0, 10)}
-                      </span>
+            {profileActivity.recent_repos.length > 0 ? (
+              <>
+                {profileActivity.recent_repos.map((repo, idx) => {
+                  return (
+                    <div className="contribution-repo" key={idx}>
+                      <div className="d-flex flex-column justify-content-start repo">
+                        <div className="d-flex flex-row justify-content-between title">
+                          <span className="title-text">{repo.repo_name}</span>
+                          <span className="commit-date">
+                            최근 커밋일자
+                            <br />
+                            {repo.committer_date.substring(0, 10)}
+                          </span>
+                        </div>
+                        <div className="d-flex flex-row justify-content-start gap-1 stat">
+                          <BsStar size={16} />
+                          <span className="stat-text">Stars {repo.stargazers_count}</span>
+                          <BsCheck2 size={16} />
+                          <span className="stat-text">Commits {repo.commits_count}</span>
+                          <BsArrow90DegLeft size={16} />
+                          <span className="stat-text">Pull Requests {repo.prs_count}</span>
+                        </div>
+                        <span className="desc">{repo.desc}</span>
+                      </div>
                     </div>
-                    <div className="d-flex flex-row justify-content-start gap-1 stat">
-                      <BsStar size={16} />
-                      <span className="stat-text">Stars {repo.stargazers_count}</span>
-                      <BsCheck2 size={16} />
-                      <span className="stat-text">Commits {repo.commits_count}</span>
-                      <BsArrow90DegLeft size={16} />
-                      <span className="stat-text">Pull Requests {repo.prs_count}</span>
-                    </div>
-                    <span className="desc">{repo.desc}</span>
-                  </div>
-                </div>
-              );
-            })}
+                  );
+                })}
+              </>
+            ) : (
+              <>최근 기여활동이 없습니다.</>
+            )}
           </div>
         </div>
       ) : (
