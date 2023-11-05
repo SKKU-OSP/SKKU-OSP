a02a127bec8b7860420220b6c3e31492370b7c25
SeoJeongYeop <sjyswe99@gmail.com>
2023-11-06 00:03:22 +0900

    feat: Board와 Team 모델의 name column에 db_index 설정 추가 - name 쿼리시 성능향상

--- osp/team/models.py
+++ osp/team/models.py
@@ -6,7 +6,7 @@ from user.models import Account
 
 class Team(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20, unique=True, db_index=True)
     description = models.CharField(max_length=150)
     image = models.ImageField(
         default='img/team/default.jpg', upload_to='img/team/')
