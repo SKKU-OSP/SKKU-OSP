e5b9e57cd2d3119181318303f7cd5c95e97fde50
SeoJeongYeop <sjyswe99@gmail.com>
2023-11-06 14:12:13 +0900

    refactor: home 앱 코드 정리

--- osp/home/admin.py
+++ osp/home/admin.py
@@ -1,19 +1,24 @@
 from django.contrib import admin
-from .models import AnnualTotal, Student, Repository
+
+from home.models import AnnualTotal, Repository, Student
 
 
 class AnnualTotalAdmin(admin.ModelAdmin):
-    list_display = ('case_num', 'student_KP', 'student_total', 'commit', 'star', 'repo', 'repo_total')
-    
+    list_display = ('case_num', 'student_KP', 'student_total',
+                    'commit', 'star', 'repo', 'repo_total')
+
+
 class StudentAdmin(admin.ModelAdmin):
     search_fields = ['github_id']
-    list_display = ('github_id', 'year', 'score', 'commit', 'star', 'pr', 'issue', 'fork')
+    list_display = ('github_id', 'year', 'score', 'commit',
+                    'star', 'pr', 'issue', 'fork')
+
 
 class RepositoryAdmin(admin.ModelAdmin):
     search_fields = ['owner']
     list_display = ('year', 'owner', 'repo_num')
 
+
 admin.site.register(AnnualTotal, AnnualTotalAdmin)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Repository, RepositoryAdmin)
-
