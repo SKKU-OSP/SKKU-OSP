a99b6ffa5a79483b03720a3f5e2591d56e247808
wjieun <wjieun@g.skku.edu>
2023-11-07 19:25:49 +0900

    feature: new alert badge

--- frontend/src/components/NavBar/Chat/ChatMessage_Container.jsx
+++ frontend/src/components/NavBar/Chat/ChatMessage_Container.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react';
-
 import axios from 'axios';
-
 import { getAuthConfig } from '../../../utils/auth';
 import ChatMessage_Presenter from './ChatMessage_Presenter';
 
