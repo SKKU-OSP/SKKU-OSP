7fa27da8791a3783b482295c305a305967a17adf
hsh <hsh200315@naver.com>
2023-11-09 11:21:16 +0900

    feat: 팀 지원서 핑크색 배지 추가

--- frontend/src/components/NavBar/TeamApplication/TeamApplication_Presenter.jsx
+++ frontend/src/components/NavBar/TeamApplication/TeamApplication_Presenter.jsx
@@ -2,16 +2,20 @@ import TeamApplication from './TeamApplication';
 import { useState } from 'react';
 import { MdOutlineAssignmentInd } from 'react-icons/md';
 
-export default function TeamApplication_Presenter({ iconSize, showTeamApp, setShowTeamApp }) {
+export default function TeamApplication_Presenter({ newAlert, iconSize, showTeamApp, setShowTeamApp }) {
   const handleClose = () => {
     setShowTeamApp(false);
   };
   const handleShow = () => {
     setShowTeamApp(true);
   };
-
   return (
     <>
+      {newAlert && (
+        <span className="badge-new badge-new-alert-application">
+          <span className="visually-hidden">New Application</span>
+        </span>
+      )}
       <MdOutlineAssignmentInd size={iconSize} onClick={handleShow} className="nav-bar-icons" />
       <TeamApplication handleClose={handleClose} show={showTeamApp} />
     </>
