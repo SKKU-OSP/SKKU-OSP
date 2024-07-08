b5e21eab9bb1bfd60e09ecab2d1cc1c3362baa0d
m1nse0kim <ms0125@g.skku.edu>
2024-07-08 22:36:11 +0900

    fix: 문의하기 모달 창을 페이지와 연결 및 백엔드 연결

--- frontend/src/components/User/TinyUser/Frame/QnAModal.jsx
+++ frontend/src/components/User/TinyUser/Frame/QnAModal.jsx
@@ -7,25 +7,35 @@ import Modal from 'react-bootstrap/Modal';
 import axios from 'axios';
 import { getAuthConfig } from '../../../../utils/auth';
 
+const server_url = import.meta.env.VITE_SERVER_URL;
+
 function QnAModal(props) {
-  const { Show, OnHandleQnAClose, OnHandleQnASaveClose, myQnA } = props;
-  const [modalQnA, setModalQnA] = useState(myQnA || { title: '', content: '' });
+  const { user, Show, OnHandleQnAClose, OnHandleQnASaveClose } = props;
+  const [modalQnA, setModalQnA] = useState({ type: '', content: '', image: [] });
   const [contentLength, setContentLength] = useState(0);
+  const [images, setImages] = useState([]);
+  const [imagePreviews, setImagePreviews] = useState([]);
 
   useEffect(() => {
-    setModalQnA(myQnA || { title: '', content: '' });
-    setContentLength(myQnA?.content.length || 0);
-  }, [Show, myQnA]);
+    setModalQnA({ type: '', content: '', image: [] });
+    setContentLength(0);
+    setImages([]);
+    setImagePreviews([]);
+  }, [Show]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
+    if (name === 'content') {
+      if (value.length > 500) {
+        alert('내용은 최대 500자까지 입력할 수 있습니다.');
+        return;
+      }
+      setContentLength(value.length);
+    }
     setModalQnA({
       ...modalQnA,
       [name]: value
     });
-    if (name === 'content') {
-      setContentLength(value.length);
-    }
   };
 
   const handleSelectChange = (selectedOption) => {
@@ -35,22 +45,87 @@ function QnAModal(props) {
     });
   };
 
-  const handleSave = async () => {
+  const handleImageChange = (e) => {
+    const files = Array.from(e.target.files);
+    if (files.length + images.length > 3) {
+      alert('최대 3개의 이미지만 업로드할 수 있습니다.');
+      return;
+    }
+
+    const newImages = files.slice(0, 3 - images.length);
+    const newImagePreviews = newImages.map((file) => URL.createObjectURL(file));
+
+    setImages([...images, ...newImages]);
+    setImagePreviews([...imagePreviews, ...newImagePreviews]);
+  };
+
+  const handleImageRemove = (index) => {
+    const newImages = images.filter((_, i) => i !== index);
+    const newImagePreviews = imagePreviews.filter((_, i) => i !== index);
+
+    setImages(newImages);
+    setImagePreviews(newImagePreviews);
+  };
+
+  const handleShowQnA = (event) => {
+    event.preventDefault();
+    console.log('modal', modalQnA);
+
+    if (modalQnA.type === '') {
+      window.alert('질문 유형을 선택해 주세요');
+      return;
+    }
+
+    if (modalQnA.content.trim() === '') {
+      window.alert('내용을 입력해 주세요');
+      return;
+    }
+
+    if (window.confirm('QnA를 등록하시겠습니까?')) {
+      postQnA();
+    }
+  };
+
+  const postQnA = async () => {
     try {
-      const adminUserId = 1; // admin user id
-      const messageContent = `[${options.find((option) => option.value === modalQnA.type)?.label}] ${modalQnA.content}`;
-      const server_url = import.meta.env.VITE_SERVER_URL;
-      const url = `${server_url}/message/api/chat/${adminUserId}/`;
+      const config = getAuthConfig();
+      config.headers['Content-Type'] = 'multipart/form-data';
 
-      const response = await axios.post(url, { 'chat-input': messageContent }, getAuthConfig());
+      const userName = user;
+      const currentDate = new Date();
+      const formattedDate = `${currentDate.getFullYear()}.${String(currentDate.getMonth() + 1).padStart(
+        2,
+        '0'
+      )}.${String(currentDate.getDate()).padStart(2, '0')}`;
 
-      if (response.status === 200) {
+      const postData = {
+        user: userName,
+        type: modalQnA.type,
+        content: modalQnA.content,
+        created_at: formattedDate,
+        solved: false
+      };
+
+      const formData = new FormData();
+      Object.entries(postData).forEach(([key, value]) => {
+        formData.append(key, value);
+      });
+
+      images.forEach((image, index) => {
+        formData.append(`image${index + 1}`, image);
+      });
+
+      const response = await axios.post(`${server_url}/user/api/qna/create/`, formData, config);
+
+      if (response.status === 201) {
+        window.alert('등록이 완료되었습니다!');
         OnHandleQnASaveClose(modalQnA);
       } else {
-        console.error('Failed to send message');
+        window.alert('등록에 실패했습니다.');
       }
     } catch (error) {
-      console.error('Error sending message:', error);
+      console.error('에러:', error);
+      window.alert('Error Occurred');
     }
   };
 
@@ -93,13 +168,37 @@ function QnAModal(props) {
               <small>{contentLength} / 500</small>
             </div>
           </Form.Group>
+          <Form.Group controlId="qnaImages" className="mt-3">
+            <Form.Label>이미지 업로드</Form.Label>
+            <Form.Control type="file" multiple accept="image/*" onChange={handleImageChange} />
+            <div className="image-previews mt-3">
+              {imagePreviews.map((preview, index) => (
+                <div key={index} style={{ position: 'relative', display: 'inline-block' }}>
+                  <img
+                    src={preview}
+                    alt={`preview-${index}`}
+                    className="img-thumbnail"
+                    style={{ maxWidth: '150px', marginRight: '10px' }}
+                  />
+                  <Button
+                    variant="secondary"
+                    size="sm"
+                    style={{ position: 'absolute', top: '5px', right: '15px' }}
+                    onClick={() => handleImageRemove(index)}
+                  >
+                    X
+                  </Button>
+                </div>
+              ))}
+            </div>
+          </Form.Group>
         </Form>
       </Modal.Body>
       <Modal.Footer className="d-flex justify-content-between align-items-center">
         <small className="text-muted">
           * 휴일을 제외한 평일에는 하루 이내에 답변 드리겠습니다. 답변은 SOSD 메시지함을 확인해주세요.
         </small>
-        <Button variant="outline-dark" onClick={handleSave}>
+        <Button variant="outline-dark" onClick={handleShowQnA}>
           제출
         </Button>
       </Modal.Footer>
