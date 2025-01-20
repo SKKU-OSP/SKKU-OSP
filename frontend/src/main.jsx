7b1f1c8b5a58f035b47757ef8177bfca486b4e8f
byungKHee <byungheekang@g.skku.edu>
2025-01-20 14:32:57 +0900

    리액트 쿼리 클라이언트 최상단 컴포넌트에 적용

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -43,6 +43,7 @@ import PasswordResetConfirm from './components/Account/PasswordResetConfirm';
 import PasswordResetComplete from './components/Account/PasswordResetComplete';
 import MainBoard_Container from './components/Community/Board/MainBoard/MainBoard_Container';
 import QnAPage from './routes/QnAPage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useEffect } from 'react';
 
 const GaTrackingId = import.meta.env.VITE_GA_TRACKING_ID;
@@ -222,8 +223,12 @@ const router = createBrowserRouter([
   }
 ]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router}>
-    <GAListner />
+    <QueryClientProvider client={queryClient}>
+      <GAListner />
+    </QueryClientProvider>
   </RouterProvider>
 );
