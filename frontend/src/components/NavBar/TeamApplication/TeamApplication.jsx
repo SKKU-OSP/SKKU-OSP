c7740f592dba700646b85a16672f41e5faa011d4
Curamy <damaon2016@naver.com>
2025-03-16 17:23:01 +0900

    대기중 상태 추가
    기본 status -> 대기중
    -1 -> 거절됨 으로 변경

--- frontend/src/components/NavBar/TeamApplication/TeamApplication.jsx
+++ frontend/src/components/NavBar/TeamApplication/TeamApplication.jsx
@@ -91,7 +91,7 @@ const TeamApplication = ({ handleClose, show }) => {
   };
 
   const handleRefuseClick = (applyId, userId) => {
-    sendTeamApplication(applyId, userId, 'false');
+    sendTeamApplication(applyId, userId, '-1');
     setReceived((prev) => prev.filter((app) => app.id !== applyId));
   };
 
@@ -131,7 +131,8 @@ const TeamApplication = ({ handleClose, show }) => {
                 <div className="d-flex">
                   <div className="me-2">
                     {' '}
-                    {application.team.name} {application.team.id}{' '}
+                    {/* {application.team.name} {application.team.id}{' '} */}
+                    {application.team.name}{' '}
                   </div>
                   <Link onClick={() => clickUsername(application.account.user.username)}>
                     {application.account.user.username}
@@ -191,13 +192,18 @@ const TeamApplication = ({ handleClose, show }) => {
               >
                 <div>{application.team.name}</div>
                 <div className="d-flex flex-row">
+                <p>현재 상태 값: {application.status ?? "알 수 없음"}</p> {/* 상태 출력 */}
                   {application.status === 1 ? (
                     <Badge bg="primary" className="text-center text-left fs-6 me-2 lh-sm">
                       승인됨
                     </Badge>
-                  ) : (
+                  ) : application.status === -1 ? (
                     <Badge bg="danger" className="text-center text-left fs-6 me-2 lh-sm">
                       거절됨
+                    </Badge>
+                  ) : (
+                    <Badge bg="secondary" className="text-center text-left fs-6 me-2 lh-sm">
+                      대기중
                     </Badge>
                   )}
                   <Button
